ec7cba40d6740ca414ff31485135d397f5876b05
Jack Sherick <sherij2@rpi.edu>
2023-02-10 17:40:34 -0500

    git commit 	modified:   1.json 	new file:   5.json 	modified:   Backend/main.py 	modified:   Backend/sort.py 	modified:   test.json

--- Backend/main.py
+++ Backend/main.py
@@ -15,8 +15,8 @@ if __name__ == "__main__":
     with open("2.json") as json_file:
         t2 = json.load(json_file)
 
-    print(sort.sortFucntion(data, t1))
-    print(sort.sortFucntion(data, t2))
+    print(sort.firstSearch(data, t1))
+    print(sort.firstSearch(data, t2))
 
     with open("test.json", "w") as out:
         json.dump(data, out, indent=4)
--- Backend/sort.py
+++ Backend/sort.py
@@ -1,41 +1,83 @@
 # what I maybe plan to do is that if there are no "perfect" matches,
 # collect rooms that are good matches and sort between those and pick
-# the best of them
+# the best of them - this sort of emulates a best fit and ideal fit at
+# the same time (will be two seperate sort functions *if a perfect fit
+# is not found*)
+
+# i want to have it working with just induvial students before I implement
+# groups 
+
 potentialRooms = []
 
+def idealSearch(student):
+
+
+
+    return
+
 # looks to see if two students have commonality
-def isMatch(room, student):
+def isPerfectMatch(room, student):
 
-    return True
+    i = 0
 
-# what does the actual sort
-def sortFucntion(data, student):
+    for i in room["occupants"]:
+        if (i["major"] == student["major"]):
+            i += 3
 
-    if (len(student["dormPref"]) != 0):
+        if (i["geo"] == student["geo"]):
+            i += 1
+
+    if (i > 3):
+        return True
+
+    return False
+
+# if there is a "perfect" fit, it matches the student there, otherwise collecting other
+# decent fits to use in a sort of ideal search
+def firstSearch(data, student):
+
+    potentialRooms.clear()
+
+    if (len(student["dormPref"]) != 0): # if there is a preferred dorm, look through those
         
         while (student["dorm"] == ""): 
-
-            i = 0               # move through dorm prefs
-            dorm = data.keys()  # list of dorms
+            
+            # move through dorm prefs  - later on we can just move the iterablle to specific points in the array
+            i = 0        
+            dorm = data.keys()  # list of dorms to use as keys
 
             for j in dorm:
                 if (student["dormPref"][i] in j):   # find preferred dorm
                     
                     # see if possible to move into room
                     if (student["sex"] == data[j]["sex"] or data[j]["sex"] == "e" and student["year"] == data[j]["year"]):
+                        if (len(data[j]["occupants"]) > 0):
+                            if (isPerfectMatch(data[j], student)):
+                                data[j]["occupants"].append(student)
+
+                                return True
+                            else:
+                                potentialRooms.append(data[j])
+
                         if (len(data[j]["occupants"]) == 0):
-                            data[j]["occupants"].append(student["name"])
-                            data[j]["sex"] = student["sex"]
+                            potentialRooms.append(data[j])
 
-                            return True
-                        if (len(data[j]["occupants"]) == 1):
-                            isMatch(data[j], student)
+            i += 1
 
+    if (len(potentialRooms) == 0):
+        dorm = data.keys()
 
-                    i += 1
-                        
+        for j in dorm:
+            if (student["sex"] == data[j]["sex"] or data[j]["sex"] == "e" and student["year"] == data[j]["year"]):
+                if (len(data[j]["occupants"]) > 0):
+                    if (isPerfectMatch(data[j], student)):
+                        data[j]["occupants"].append(student)
 
+                        return True
+                    else:
+                        potentialRooms.append(data[j])
 
-                    
+                if (len(data[j]["occupants"]) == 0):
+                    potentialRooms.append(data[j])
                
-    return False
+    idealSearch(student)
