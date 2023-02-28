bf83b9142811751f68db2844ad58d14d89a70cd5
ShadowSkeleton <fengj5@rpi.edu>
2023-02-28 17:31:56 -0500

    bootstrap in vite

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import { Button } from 'react-bootstrap';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -15,18 +16,26 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React with ypescript + Bootstrap 5</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Button variant="primary">Primary</Button>{' '}
+      <Button variant="secondary">Secondary</Button>{' '}
+      <Button variant="success">Success</Button>{' '}
+      <Button variant="warning">Warning</Button>{' '}
+      <Button variant="danger">Danger</Button>{' '}
+      <Button variant="info">Info</Button>{' '}
+      <Button variant="light">Light</Button>{' '}
+      <Button variant="dark">Dark</Button> <Button variant="link">Link</Button>
     </div>
   )
 }
