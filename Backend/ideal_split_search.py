a83381931274d0ad9dc1a015c45b25fd1a350c75
Mdong9 <mikedongm13@gmail.com>
2023-03-03 21:02:54 -0500

    changed ideal split seach to fit updated campus structure

--- Backend/ideal_split_search.py
+++ Backend/ideal_split_search.py
@@ -1,3 +1,5 @@
+import json
+import copy
 # Implement Algorithm Here:
 
 # def idealSplitSearch():
@@ -17,19 +19,18 @@ def ideal_split_search(group_list,group_preferences,campus):
   group2 = group_list[middle_index:]
 
   for dorm in group_preferences:#loops through all the dorms in the groups preferred list
-    for floor in campus[dorm]: #loops through the floors of said building
-      for room in campus[dorm][floor]:#loops through the rooms in the building
-        #if the room size does not fit the group or it is occupied
-        if campus[dorm][floor][room]["size"] != len(group1) or len(campus[dorm][floor][room]["Occupants"]) > 0: 
-          continue
-        else:
-          if campus[dorm][floor][room]["shared_bathroom"] != False:
-            connected_room = campus[dorm][floor][room]["shared_bathroom"]
-            if campus[dorm][floor][connected_room]["size"] == len(group2) or len(campus[dorm][floor][room]["Occupants"]) > 0:
-              campus[dorm][floor][room]["Occupants"] = copy.deepcopy(group1)
-              campus[dorm][floor][room]["Occupants"] = copy.deepcopy(group2)
-              a = open('C:/Users/dongm2/Documents/RPI/Personal_projects/Rcos/objects_changed.json', 'w')
-              json.dump(campus,a, indent = 2)
-              a.close()
-              return 1
+    for room in campus[dorm]:#loops through the rooms in the building
+      #if the room size does not fit the group or it is occupied
+      if campus[dorm][room]["size"] != len(group1) or len(campus[dorm][room]["Occupants"]) > 0: 
+        continue
+      else:
+        if campus[dorm][room]["shared_bathroom"] != False:
+          connected_room = campus[dorm][room]["shared_bathroom"]
+          if campus[dorm][connected_room]["size"] == len(group2) or len(campus[dorm][room]["Occupants"]) > 0:
+            campus[dorm][room]["Occupants"] = copy.deepcopy(group1)
+            campus[dorm][room]["Occupants"] = copy.deepcopy(group2)
+            a = open('C:/Users/dongm2/Documents/RPI/Personal_projects/Rcos/objects_changed.json', 'w')
+            json.dump(campus,a, indent = 2)
+            a.close()
+            return 1
   return 0
