b5059bd4a160a4b985c1040485520d8f6927b1ce
Kenneth Dong <dongkenneth256@gmail.com>
2023-03-17 16:47:32 -0400

    updated contacts page

--- Frontend/src/contact.jsx
+++ Frontend/src/contact.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import './resources/style.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import $ from 'jquery';
