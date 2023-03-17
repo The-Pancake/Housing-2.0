a22edde2b890b56f2486ecd13a65f423aa358417
MeiH10 <huangm10@rpi.edu>
2023-03-17 18:32:51 -0400

    made image square and added description

--- frontend/src/Profile.jsx
+++ frontend/src/Profile.jsx
@@ -23,10 +23,10 @@ const Profile = () => {
                 <div className="row bg-primary full-height">
                     <div className="col-sm-4 bg-warning">
 
-                    <div className="square-img">
-                        <img src={profile_img} alt="profile" id="profile" />
+                    <div className="square-img ">
+                        <img src={profile_img} alt="profile" className="ratio ratio-1x1" id="profile" fluid/>
                     </div>
-                    <p className="text-center">Your description here</p>
+                    <p className="text-center">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt qui corporis, doloremque consequuntur soluta officia corrupti deleniti architecto maxime asperiores perspiciatis explicabo sed! Necessitatibus vel at consequatur cupiditate aut repudiandae tenetur delectus ab sit autem placeat recusandae consequuntur, inventore temporibus animi assumenda nemo suscipit quia modi deserunt, impedit molestias nulla!</p>
 
                     </div>
                     <div className="col-sm-4">Column 2</div>
