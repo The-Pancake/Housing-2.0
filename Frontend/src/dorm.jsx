7fc67e596674268e90b47076124a0016f8b1f3f3
Kenneth Dong <dongkenneth256@gmail.com>
2023-03-21 16:58:59 -0400

    added filler information

--- Frontend/src/dorm.jsx
+++ Frontend/src/dorm.jsx
@@ -1,10 +1,87 @@
 import React from 'react';
 import { useState } from "react";
 
-const DormGrid = ({hidden}) => {
+const DormGrid = ({ hidden }) => {
   return (
     <div hidden={hidden}>
-      <h1>Dorms</h1>
+      <div class="row row-cols-1 row-cols-md-3 g-4">
+        <div class="col">
+          <div class="card h-100">
+            <div class="card-body">
+              <h5 class="card-title">BARH</h5>
+              <p class="card-text">Some information about BARH here</p>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card h-100">
+            <div class="card-body">
+              <h5 class="card-title">BARH</h5>
+              <p class="card-text">Some information about BARH here</p>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card h-100">
+            <div class="card-body">
+              <h5 class="card-title">BARH</h5>
+              <p class="card-text">Some information about BARH here</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row row-cols-1 row-cols-md-3 g-4">
+        <div class="col">
+          <div class="card h-100">
+            <div class="card-body">
+              <h5 class="card-title">BARH</h5>
+              <p class="card-text">Some information about BARH here</p>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card h-100">
+            <div class="card-body">
+              <h5 class="card-title">BARH</h5>
+              <p class="card-text">Some information about BARH here</p>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card h-100">
+            <div class="card-body">
+              <h5 class="card-title">BARH</h5>
+              <p class="card-text">Some information about BARH here</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row row-cols-1 row-cols-md-3 g-4">
+        <div class="col">
+          <div class="card h-100">
+            <div class="card-body">
+              <h5 class="card-title">BARH</h5>
+              <p class="card-text">Some information about BARH here</p>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card h-100">
+            <div class="card-body">
+              <h5 class="card-title">BARH</h5>
+              <p class="card-text">Some information about BARH here</p>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card h-100">
+            <div class="card-body">
+              <h5 class="card-title">BARH</h5>
+              <p class="card-text">Some information about BARH here</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
