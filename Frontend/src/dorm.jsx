06ee0995e6078b24c5e8424203fa6199bca4891e
Kenneth Dong <dongkenneth256@gmail.com>
2023-03-28 16:37:21 -0400

    made navbar sticky to top

--- Frontend/src/dorm.jsx
+++ Frontend/src/dorm.jsx
@@ -1,87 +1,33 @@
-import React from 'react';
-import { useState } from "react";
+import React, { useState, useEffect } from 'react';
 
 const DormGrid = ({ hidden }) => {
+  const [dorms, setDorms] = useState([]);
+
+  useEffect(() => {
+    fetch('../campus.json')
+      .then(response => response.json())
+      .then(data => setDorms(data))
+      .catch(error => console.error(error));
+      console.log(dorms);
+  }, []);
+
   return (
     <div hidden={hidden}>
-      <div class="row row-cols-1 row-cols-md-3 g-4">
-        <div class="col">
-          <div class="card h-100">
-            <div class="card-body">
-              <h5 class="card-title">BARH</h5>
-              <p class="card-text">Some information about BARH here</p>
+      {dorms.length === 0 && <p>Loading dorm data...</p>}
+      {dorms.length > 0 && (
+        <>
+          {dorms.map(dorm => (
+            <div className="col" key={dorm.name}>
+              <div className="card h-100">
+                <div className="card-body">
+                  <h5 className="card-title">{dorm.rooms}</h5>
+                  <p className="card-text">{dorm.price}</p>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-        <div class="col">
-          <div class="card h-100">
-            <div class="card-body">
-              <h5 class="card-title">BARH</h5>
-              <p class="card-text">Some information about BARH here</p>
-            </div>
-          </div>
-        </div>
-        <div class="col">
-          <div class="card h-100">
-            <div class="card-body">
-              <h5 class="card-title">BARH</h5>
-              <p class="card-text">Some information about BARH here</p>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="row row-cols-1 row-cols-md-3 g-4">
-        <div class="col">
-          <div class="card h-100">
-            <div class="card-body">
-              <h5 class="card-title">BARH</h5>
-              <p class="card-text">Some information about BARH here</p>
-            </div>
-          </div>
-        </div>
-        <div class="col">
-          <div class="card h-100">
-            <div class="card-body">
-              <h5 class="card-title">BARH</h5>
-              <p class="card-text">Some information about BARH here</p>
-            </div>
-          </div>
-        </div>
-        <div class="col">
-          <div class="card h-100">
-            <div class="card-body">
-              <h5 class="card-title">BARH</h5>
-              <p class="card-text">Some information about BARH here</p>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="row row-cols-1 row-cols-md-3 g-4">
-        <div class="col">
-          <div class="card h-100">
-            <div class="card-body">
-              <h5 class="card-title">BARH</h5>
-              <p class="card-text">Some information about BARH here</p>
-            </div>
-          </div>
-        </div>
-        <div class="col">
-          <div class="card h-100">
-            <div class="card-body">
-              <h5 class="card-title">BARH</h5>
-              <p class="card-text">Some information about BARH here</p>
-            </div>
-          </div>
-        </div>
-        <div class="col">
-          <div class="card h-100">
-            <div class="card-body">
-              <h5 class="card-title">BARH</h5>
-              <p class="card-text">Some information about BARH here</p>
-            </div>
-          </div>
-        </div>
-      </div>
+          ))}
+        </>
+      )}
     </div>
   );
 };
