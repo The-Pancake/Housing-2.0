da6b4ad20517434f9d7c20407a7ed4b1081c349a
MeiH10 <huangm10@rpi.edu>
2023-03-31 22:49:55 -0400

    fixed the padding of the contact form in the contact us page

--- Frontend/src/contact.jsx
+++ Frontend/src/contact.jsx
@@ -6,13 +6,13 @@ const ContactUs = ({hidden}) => {
   return (
     <div hidden={hidden}>
       <h1></h1>
-      <h1>Contact Us</h1>
-      <form className={"contact-form"}>
-        <label htmlFor="name">Name:</label><br />
+      <h1 className='m-5'>Contact Us</h1>
+      <form className={"contact-form m-5"}>
+        <label htmlFor="name" className='text-white'>Name:</label><br />
         <input type="text" id="name" name="name" /><br />
-        <label htmlFor="email">Email:</label><br />
+        <label htmlFor="email" className='text-white'>Email:</label><br />
         <input type="email" id="email" name="email" /><br />
-        <label htmlFor="message">Message:</label><br />
+        <label htmlFor="message" className='text-white'>Message:</label><br />
         <textarea id="message" name="message"></textarea><br /><br />
         <input type="submit" value="Submit" />
       </form>
