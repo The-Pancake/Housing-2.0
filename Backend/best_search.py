98b82d15b98bf0165a229f94caa4640d036e5c0f
yashicac <91489374+yashicac@users.noreply.github.com>
2023-03-31 21:21:35 -0400

    Added test code for class initialization

--- Backend/best_search.py
+++ Backend/best_search.py
@@ -12,3 +12,25 @@ class Rooms:
 
 def bestSearch():
     return
+
+if __name__ == "__main__":
+    
+    #reading data from JSON file
+    with open("test.json") as json_file:
+        data = json.load(json_file)
+        
+        
+    #creating an array of objects with all the data in test file
+    allrooms = []
+    
+    for room in data:
+        currRoom = data[room]
+        name = currRoom['name']
+        gender = currRoom['sex']
+        num = currRoom['id']
+        bathroom = False;
+        size = currRoom['size']
+        year = currRoom['year']
+        occupants = currRoom['occupants']
+        newRoom = Rooms(name, gender, num, size, bathroom, year, occupants)
+        allrooms.append(newRoom)
