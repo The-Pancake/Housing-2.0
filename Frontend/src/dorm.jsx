771b380f9a9cc0ca4eee1cb61522d50d3e68f1b0
Kenneth Dong <dongkenneth256@gmail.com>
2023-04-04 17:01:35 -0400

    added bootstrap styling

--- Frontend/src/dorm.jsx
+++ Frontend/src/dorm.jsx
@@ -15,39 +15,45 @@ const DormGrid = ({ hidden }) => {
 
   return (
     <div hidden={hidden}>
-      <div className="grid-container">
-        {dormsData.dorms.map((dorm) => (
-          <div className="card" key={dorm.id}>
-            <h2>{dorm.name}</h2>
-            <p>Type: {dorm.type}</p>
-            <p>Rooms: {dorm.rooms.join(", ")}</p>
-            <p>Price: {dorm.price.join(", ")}</p>
-            <p>Students per suite: {dorm.students_per_suite}</p>
-            <p>Number of occupants: {dorm.numOccu}</p>
-            <p>Number of floors: {dorm.numFloor}</p>
-            <p>Inclusive: {dorm.inclusive}</p>
-            <p>Restroom: {dorm.restroom}</p>
-            <p>Cleaning: {dorm.cleaning}</p>
-            <p>Schedule: {dorm.schedule}</p>
-            <p>Furniture:</p>
-            <ul>
-              {Object.entries(dorm.furniture).map(([name, value]) => (
-                <li key={name}>
-                  {name}: {Array.isArray(value) ? value.join(" ") : value}
-                </li>
-              ))}
-            </ul>
-            <p>Amenities:</p>
-            <ul>
-              {Object.entries(dorm.amenities).map(([name, value]) => (
-                <li key={name}>
-                  {name}: {Array.isArray(value) ? value.join(" ") : value}
-                </li>
-              ))}
-            </ul>
-            <p>Nearby: {dorm.nearby}</p>
-          </div>
-        ))}
+      <div className="container">
+        <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
+          {dormsData.dorms.map((dorm) => (
+            <div className="col" key={dorm.id}>
+              <div className="card h-100">
+                <div className="card-body">
+                  <h5 className="card-title">{dorm.name}</h5>
+                  <p className="card-text">Type: {dorm.type}</p>
+                  <p className="card-text">Rooms: {dorm.rooms.join(", ")}</p>
+                  <p className="card-text">Price: {dorm.price.join(", ")}</p>
+                  <p className="card-text">Students per suite: {dorm.students_per_suite}</p>
+                  <p className="card-text">Number of occupants: {dorm.numOccu}</p>
+                  <p className="card-text">Number of floors: {dorm.numFloor}</p>
+                  <p className="card-text">Inclusive: {dorm.inclusive}</p>
+                  <p className="card-text">Restroom: {dorm.restroom}</p>
+                  <p className="card-text">Cleaning: {dorm.cleaning}</p>
+                  <p className="card-text">Schedule: {dorm.schedule}</p>
+                  <p className="card-text">Furniture:</p>
+                  <ul className="card-text">
+                    {Object.entries(dorm.furniture).map(([name, value]) => (
+                      <li key={name}>
+                        {name}: {Array.isArray(value) ? value.join(" ") : value}
+                      </li>
+                    ))}
+                  </ul>
+                  <p className="card-text">Amenities:</p>
+                  <ul className="card-text">
+                    {Object.entries(dorm.amenities).map(([name, value]) => (
+                      <li key={name}>
+                        {name}: {Array.isArray(value) ? value.join(" ") : value}
+                      </li>
+                    ))}
+                  </ul>
+                  <p className="card-text">Nearby: {dorm.nearby}</p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
