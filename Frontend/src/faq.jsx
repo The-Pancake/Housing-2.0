db0069b46c3b0abdc1490468472f22f1494916fd
tenzinpalden <tpalden2003@gmail.com>
2023-04-10 18:27:22 -0400

    FAQ Update
    Co-Authored-By: David Vasquez <36971642+vasquezd21@users.noreply.github.com>

--- Frontend/src/faq.jsx
+++ Frontend/src/faq.jsx
@@ -1,98 +1,72 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './faq.css';
+import React, { useState } from 'react';
+import './FAQ.css';
 
-const faq = ({hidden}) => {
+const faq = () => {
+  const [open, setOpen] = useState(null);
+
+  const toggleAnswer = (index) => {
+    if (open === index) {
+      setOpen(null);
+    } else {
+      setOpen(index);
+    }
+  };
+
+  const faqs = [
+    {
+      question: 'When will the Housing Application be open?',
+      answer: 'April 15, 2021.',
+    },
+    {
+      question: 'Will I be able to pre-select a roommate?',
+      answer: 'During the housing application process, you will be able to select a preferred roommate/suitemate group. Placement in not guaranteed, however the staff in the Student Living and Learning Office will do their very best to match preferred roommates/suitemates.',
+    },
+    {
+      question: 'What if I do not pre-select a roommate in the application?',
+      answer: 'During the housing application process, you will be able to fill out a Roommate Matching Survey and will be able to search for roommates based on answers to their questions. If you do not pre-select a roommate, you will be assigned a roommate based on your answers to the survey.',
+    },
+    {
+      question: 'Is Gender Inclusive Housing an option for students?',
+      answer: 'Recognizing that single-gender housing may not be appropriate or comfortable for all students, there are a limited number of rooms made available as Gender Inclusive Housing in both the residence halls and apartments, on a space-available basis. Students signing up for these areas will be permitted to have roommates and suitemates that identify across the gender spectrum. You may choose this option in the housing application.',
+    },
+    {
+      question: 'What housing options will be available to first-year students?',
+      answer: 'As a part of CLASS: Clustered Learning, Advocacy, and Support for Students – the student experience at Rensselaer - students live with others from their cohort. This optimizes your transition by maximizing social opportunities via the living and learning experience. A list of first-year residence halls can be found here.',
+    },
+    {
+      question: 'What criteria is utilized to decide room selection?',
+      answer: 'Housing preferences are accommodated based on deposit paid date. In other words, the sooner an incoming student submits their deposit, the more likely it is that student will receive their preferred roommate and residence hall.',
+    },
+    {
+      question: 'How do I learn more about the amenities and features of each residence hall?',
+      answer: 'The Student Living and Learning website provides amenity information about each of our halls including furniture (and dimensions), air-conditioning (if available), hall carpeting, elevators, laundry, in-suite kitchens (for apartments), and nearest dining hall.',
+    },
+    {
+      question: 'What are my options if I have a medical condition or unique situation that will require a special housing accommodation?',
+      answer: 'If you are in need of a housing accommodation, complete the housing accommodation form. Please note that you will be asked to provide medical documentation for cases of medical conditions.',
+    },
+    {
+      question: 'What are Special Interest Housing communities and can first-year students enroll in these communities?',
+      answer: 'Special Interest Housing communities offer the opportunity to extend the classroom learning experience to a living environment shared with other students with similar interests and passions. For more information, visit the Student Living and Learning website.',
+    },
+    {
+      question: 'When will the housing application close?',
+      answer: 'Please complete your application by May 24, 2021, to ensure the best chance to be assigned your preferred housing. Students who do not complete the application by May 24, 2021, will still receive a housing assignment, but they may miss the opportunity to be placed with preferred roommates/suitemates or in a preferred residence hall.',
+    },
+  ];
+
   return (
-    <div hidden={hidden}>
-    <h1 className='m-5'>Frequently Asked Questions</h1>
-    <div className='container'>
-      <div className='accordion'>
-        <div className='accordion-item'>
-          <div
-            className='accordion-item-header'
-            data-bs-toggle='collapse'
-            data-bs-target='#collapse1'
-          >
-             When will the Housing Application be open?
+    <div className="FAQ">
+      <h1>Frequently Asked Questions</h1>
+      {faqs.map((faq, index) => (
+        <div key={index}>
+          <div className="question" onClick={() => toggleAnswer(index)}>
+            {faq.question}
           </div>
-          <div id='collapse1' className='accordion-collapse collapse'>
-            <div className='accordion-body'>
-            April 15, 2021. 
-            </div>
-          </div>
+          {open === index && <div className="answer">{faq.answer}</div>}
         </div>
-        <div className='accordion-item'>
-          <div
-            className='accordion-item-header'
-            data-bs-toggle='collapse'
-            data-bs-target='#collapse2'
-          >
-            Will I be able to pre-select a roommate?
-          </div>
-          <div id='collapse2' className='accordion-collapse collapse'>
-            <div className='accordion-body'>
-            During the housing application process, you will be able to select a preferred roommate/suitemate group. Placement in not guaranteed, however the staff in the Student Living and Learning Office will do their very best to match preferred roommates/suitemates. 
-            </div>
-          </div>
-        </div>
-        <div className='accordion-item'>
-          <div
-            className='accordion-item-header'
-            data-bs-toggle='collapse'
-            data-bs-target='#collapse3'
-          >
-            When will the housing application close?
-          </div>
-          <div id='collapse3' className='accordion-collapse collapse'>
-            <div className='accordion-body'>
-            Please complete your application by May 24, 2021, to ensure the best chance to be assigned your preferred housing. Students who do not complete the application by May 24, 2021, will still receive a housing assignment, but they may miss the opportunity to be placed with preferred roommates/suitemates or in a preferred residence hall.
-            </div>
-          </div>
-        </div>
-        <div className='accordion-item'>
-          <div
-            className='accordion-item-header'
-            data-bs-toggle='collapse'
-            data-bs-target='#collapse3'
-          >
-            Will I be required to have a meal plan?
-          </div>
-          <div id='collapse3' className='accordion-collapse collapse'>
-            <div className='accordion-body'>
-            All first-year students will be required to have a meal plan for the Fall 2021 semester. Meal plan options will be communicated upon the release of the housing application.            </div>
-          </div>
-        </div>
-        <div className='accordion-item'>
-          <div
-            className='accordion-item-header'
-            data-bs-toggle='collapse'
-            data-bs-target='#collapse3'
-          >
-            Am I able to bring a car to campus?
-          </div>
-          <div id='collapse3' className='accordion-collapse collapse'>
-            <div className='accordion-body'>
-            First-year students may not bring a car to campus and will not be able to purchase a parking pass.
-            </div>
-          </div>
-        </div>
-        <div className='accordion-item'>
-          <div
-            className='accordion-item-header'
-            data-bs-toggle='collapse'
-            data-bs-target='#collapse3'
-          >
-            What housing options will be available to first-year students?
-          </div>
-          <div id='collapse3' className='accordion-collapse collapse'>
-            <div className='accordion-body'>
-            As a part of CLASS: Clustered Learning, Advocacy, and Support for Students – the student experience at Rensselaer - students live with others from their cohort. This optimizes your transition by maximizing social opportunities via the living and learning experience. A list of first-year residence halls can be found here.            </div>
-          </div>
-        </div>
-      </div>
+      ))}
     </div>
-  </div>
   );
 };
 
