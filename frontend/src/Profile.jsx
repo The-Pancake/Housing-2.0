f087a5da1bf6802ddd5f0d7f4f41e72aede96df7
MeiH10 <huangm10@rpi.edu>
2023-04-11 23:36:49 -0400

    changed photo, description, font size and font spacing

--- frontend/src/Profile.jsx
+++ frontend/src/Profile.jsx
@@ -11,13 +11,16 @@ const Profile = ({hidden}) => {
                 <div className="row full-height">
                     <div className="col-sm-4 p-0">
 
-                        <div className="card p-3 m-3 profile-card cool-bg">
-                            <img src={profile_img} alt="profile" className="card-img-top ratio ratio-1x1 mt-2" id="profile" fluid/>
-                            <div class="card-body m-0 p-0 pt-3">
-                                <h5 class="card-title text-white">Ben Bittidle</h5>
-                                <p class="card-text text-white">Hey there, I'm Ben Bittidle, a psychology student and a proud anime otaku. When I'm not studying or playing video games, you'll find me binge-watching anime or reading manga. I'm a bit of a nerd, but I'm always down for some friendly competition and socializing. In terms of living habits, I'm pretty low-maintenance and tidy. I'm respectful of my roommates' privacy and belongings, and I expect the same in return. I'm looking for like-minded otaku roommates to share a living space with. If you're into anime, gaming, and all things nerdy, then let's connect and see if we'd make great roommates!</p>
-                            </div>
-                        </div>
+<div className="card p-3 m-3 profile-card cool-bg">
+    <div class="text-center">
+        <img src={profile_img} alt="profile" className="card-img-top ratio mt-2" id="profile" fluid/>
+    </div>
+    <div class="card-body m-0 p-0 pt-3">
+        <h5 class="card-title text-white">Ben Bittidle</h5>
+        <p class="card-text text-white">Hiiii! I'm Ben-senpai! I'm a kawaii psychology student who loves all things anime and video games! When I'm not studying, I'm watching anime and reading manga, nya~! I'm a bit of an otaku, but I'm always up for some friendly competition and hanging out with my fellow otaku senpais! As for my living habits, I'm pretty easy-going and tidy! I always respect my roommates' privacy and belongings, and I hope they'll do the same for me, nya~! I'm looking for like-minded otaku senpais to share a living space with, so if you're into anime, gaming, and all things nerdy, let's connect and see if we'd make great roommates, uwu~!</p>
+    </div>
+</div>
+
                     </div>
                     <div className="col-sm-4 mt-3 text-white">
 
