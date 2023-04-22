c1e4c62e5df9beb694e4d9364ebf778393040255
Kenneth Dong <dongkenneth256@gmail.com>
2023-04-21 21:21:01 -0400

    fixed formatting

--- Frontend/src/dorm.jsx
+++ Frontend/src/dorm.jsx
@@ -20,53 +20,70 @@ const DormGrid = ({ hidden }) => {
         console.log(dorms);
     }, []);
 
-  return (
-    <div hidden={hidden}>
-      <div className="grid-container">
-        {dormsData.dorms.map((dorm) => (
-          <div className="card cool-bg text-white" key={dorm.id}>
-            <h2>{dorm.name}</h2>
-            <p>Type: {dorm.type}</p>
-            <button onClick={() => setSelectedDorm(dorm)}>Show more</button>
-          </div>
-        ))}
-      </div>
-      <Modal show={selectedDorm !== null} onHide={() => setSelectedDorm(null)}>
-        <Modal.Header closeButton>
-          <Modal.Title>{selectedDorm?.name}</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <p>Type: {selectedDorm?.type}</p>
-          <p>Rooms: {selectedDorm?.rooms.join(", ")}</p>
-          <p>Price: {selectedDorm?.price.join(", ")}</p>
-          <p>Students per suite: {selectedDorm?.students_per_suite}</p>
-          <p>Number of occupants: {selectedDorm?.numOccu}</p>
-          <p>Number of floors: {selectedDorm?.numFloor}</p>
-          <p>Inclusive: {selectedDorm?.inclusive}</p>
-          <p>Restroom: {selectedDorm?.restroom}</p>
-          <p>Cleaning: {selectedDorm?.cleaning}</p>
-          <p>Schedule: {selectedDorm?.schedule}</p>
-          <p>Furniture:</p>
-          <ul>
-            {Object.entries(selectedDorm?.furniture || {}).map(([name, value]) => (
-              <li key={name}>
-                {name}: {Array.isArray(value) ? value.join(" ") : value}
-              </li>
-            ))}
-          </ul>
-          <p>Amenities:</p>
-          <ul>
-            {Object.entries(selectedDorm?.amenities || {}).map(([name, value]) => (
-              <li key={name}>
-                {name}: {Array.isArray(value) ? value.join(" ") : value}
-              </li>
-            ))}
-          </ul>
-          <p>Nearby: {selectedDorm?.nearby}</p>
-        </Modal.Body>
-      </Modal>
-    </div>
-  );
+    return (
+        <div hidden={hidden}>
+            <div className="grid-container">
+                {dormsData.dorms.map((dorm) => (
+                    <div className="card cool-bg text-white" key={dorm.id}>
+                        <h2>{dorm.name}</h2>
+                        <p>Type: {dorm.type}</p>
+                        <button onClick={() => setSelectedDorm(dorm)}>
+                            Show more
+                        </button>
+                    </div>
+                ))}
+            </div>
+            <Modal
+                show={selectedDorm !== null}
+                onHide={() => setSelectedDorm(null)}
+            >
+                <Modal.Header closeButton>
+                    <Modal.Title>{selectedDorm?.name}</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <p>Type: {selectedDorm?.type}</p>
+                    <p>Rooms: {selectedDorm?.rooms.join(", ")}</p>
+                    <p>Price: {selectedDorm?.price.join(", ")}</p>
+                    <p>
+                        Students per suite: {selectedDorm?.students_per_suite}
+                    </p>
+                    <p>Number of occupants: {selectedDorm?.numOccu}</p>
+                    <p>Number of floors: {selectedDorm?.numFloor}</p>
+                    <p>Inclusive: {selectedDorm?.inclusive}</p>
+                    <p>Restroom: {selectedDorm?.restroom}</p>
+                    <p>Cleaning: {selectedDorm?.cleaning}</p>
+                    <p>Schedule: {selectedDorm?.schedule}</p>
+                    <p>Furniture:</p>
+                    <ul>
+                        {Object.entries(selectedDorm?.furniture || {}).map(
+                            ([name, value]) => (
+                                <li key={name}>
+                                    {name}:{" "}
+                                    {Array.isArray(value)
+                                        ? value.join(" ")
+                                        : value}
+                                </li>
+                            )
+                        )}
+                    </ul>
+                    <p>Amenities:</p>
+                    <ul>
+                        {Object.entries(selectedDorm?.amenities || {}).map(
+                            ([name, value]) => (
+                                <li key={name}>
+                                    {name}:{" "}
+                                    {Array.isArray(value)
+                                        ? value.join(" ")
+                                        : value}
+                                </li>
+                            )
+                        )}
+                    </ul>
+                    <p>Nearby: {selectedDorm?.nearby}</p>
+                </Modal.Body>
+            </Modal>
+        </div>
+    );
 };
 
 export default DormGrid;
--- Frontend/src/roommatePairing.jsx
+++ Frontend/src/roommatePairing.jsx
@@ -1,85 +1,108 @@
-import React, { useState } from 'react';
-import { ProgressBar, Form, Button } from 'react-bootstrap';
+import React, { useState } from "react";
+import { ProgressBar, Form, Button } from "react-bootstrap";
 import "./roommatePairing.css";
 
-
 const RoommatePairing = ({ hidden }) => {
-  const [progress, setProgress] = useState(0);
-  const [answers, setAnswers] = useState([]);
+    const [progress, setProgress] = useState(0);
+    const [answers, setAnswers] = useState([]);
 
-  const questions = [
-    {
-      text: "What's your living style?",
-      choices: ["Neat and tidy", "Relaxed and casual", "A bit of both"]
-    },
-    {
-      text: "What time do you usually wake up?",
-      choices: ["Early bird", "Night owl", "It varies"]
-    },
-    {
-        text: "What's your favorite food?",
-        choices: ["Chinese", "Japanese", "Korean", "Western", "Indian", "Thai", "Vietnamese", "Other"]
-    },
-    {
-        text: "What's your favorite drink?",
-        choices: ["Coffee", "Tea", "Juice", "Milk", "Water", "Other"]
-    }
-    // Add 10 more questions here
-  ];
+    const questions = [
+        {
+            text: "What's your living style?",
+            choices: ["Neat and tidy", "Relaxed and casual", "A bit of both"],
+        },
+        {
+            text: "What time do you usually wake up?",
+            choices: ["Early bird", "Night owl", "It varies"],
+        },
+        {
+            text: "What's your favorite food?",
+            choices: [
+                "Chinese",
+                "Japanese",
+                "Korean",
+                "Western",
+                "Indian",
+                "Thai",
+                "Vietnamese",
+                "Other",
+            ],
+        },
+        {
+            text: "What's your favorite drink?",
+            choices: ["Coffee", "Tea", "Juice", "Milk", "Water", "Other"],
+        },
+        // Add 10 more questions here
+    ];
 
-  const handleAnswer = (answer) => {
-    setProgress(progress + 1);
-    setAnswers([...answers.slice(0, progress), answer]);
-    const radios = document.getElementsByName('answer');
-    for (let i = 0; i < radios.length; i++) {
-        radios[i].checked = false;
+    const handleAnswer = (answer) => {
+        setProgress(progress + 1);
+        setAnswers([...answers.slice(0, progress), answer]);
+        const radios = document.getElementsByName("answer");
+        for (let i = 0; i < radios.length; i++) {
+            radios[i].checked = false;
+        }
+    };
 
-    }
-  };
+    const handleRedo = () => {
+        setProgress(0);
+        setAnswers([]);
+    };
 
-  const handleRedo = () => {
-    setProgress(0);
-    setAnswers([]);
-  };
-
-  const currentQuestion = questions[progress];
-  return (
-     <div hidden={hidden}>
-  <div className="row">
-    <div className="col-lg-8 offset-lg-2">
-      <div className="progress mt-5 mb-5 bg-light">
-        <div
-          className="progress-bar bg-primary"
-          role="progressbar"
-          style={{ width: `${(progress / questions.length) * 100}%` }}
-          aria-valuenow={progress}
-          aria-valuemin="0"
-          aria-valuemax={questions.length}
-        ></div>
-      </div>
-      {currentQuestion && (
-        <Form className="mt-5 mb-5 text-white" style={{ backgroundColor: 'rgba(33, 37, 41, 0.65)'}}>
-          <Form.Group >
-            <Form.Label className="h5 m-0">{currentQuestion.text}</Form.Label>
-            {currentQuestion.choices.map((choice) => (
-              <Form.Check
-                type="radio"
-                label={choice}
-                name="answer"
-                onChange={() => handleAnswer(choice)}
-                className="h5 m-2 my-3"
-              />
-            ))}
-          </Form.Group>
-        </Form>
-      )}
-      <Button onClick={handleRedo} variant="primary" size="lg" className="mt-3">
-        Redo
-      </Button>
-    </div>
-  </div>
-</div>
-  );
+    const currentQuestion = questions[progress];
+    return (
+        <div hidden={hidden}>
+            <div className="row">
+                <div className="col-lg-8 offset-lg-2">
+                    <div className="progress mt-5 mb-5 bg-light">
+                        <div
+                            className="progress-bar bg-primary"
+                            role="progressbar"
+                            style={{
+                                width: `${
+                                    (progress / questions.length) * 100
+                                }%`,
+                            }}
+                            aria-valuenow={progress}
+                            aria-valuemin="0"
+                            aria-valuemax={questions.length}
+                        ></div>
+                    </div>
+                    {currentQuestion && (
+                        <Form
+                            className="mt-5 mb-5 text-white"
+                            style={{
+                                backgroundColor: "rgba(33, 37, 41, 0.65)",
+                            }}
+                        >
+                            <Form.Group>
+                                <Form.Label className="h5 m-0">
+                                    {currentQuestion.text}
+                                </Form.Label>
+                                {currentQuestion.choices.map((choice) => (
+                                    <Form.Check
+                                        type="radio"
+                                        label={choice}
+                                        name="answer"
+                                        onChange={() => handleAnswer(choice)}
+                                        className="h5 m-2 my-3"
+                                    />
+                                ))}
+                            </Form.Group>
+                        </Form>
+                    )}
+                    <Button
+                        onClick={handleRedo}
+                        variant="primary"
+                        size="lg"
+                        className="mt-3"
+                    >
+                        Redo
+                    </Button>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default RoommatePairing;
