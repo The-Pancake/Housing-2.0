dd03df8fe94b69793ef037e817b612e9ff5cb8bc
ShadowSkeleton <fengj5@rpi.edu>
2023-04-21 21:19:09 -0400

    update roommate pairing system with external json file
    create a question json file and make roommate pairing using this json file

--- Frontend/src/roommatePairing.jsx
+++ Frontend/src/roommatePairing.jsx
@@ -1,39 +1,17 @@
-import React, { useState } from 'react';
-import { ProgressBar, Form, Button } from 'react-bootstrap';
-import "./roommatePairing.css";
-
+import { useState } from 'react';
+import { Form, Button } from 'react-bootstrap';
+import questions from './quiz.json';
 
 const RoommatePairing = ({ hidden }) => {
   const [progress, setProgress] = useState(0);
   const [answers, setAnswers] = useState([]);
-
-  const questions = [
-    {
-      text: "What's your living style?",
-      choices: ["Neat and tidy", "Relaxed and casual", "A bit of both"]
-    },
-    {
-      text: "What time do you usually wake up?",
-      choices: ["Early bird", "Night owl", "It varies"]
-    },
-    {
-        text: "What's your favorite food?",
-        choices: ["Chinese", "Japanese", "Korean", "Western", "Indian", "Thai", "Vietnamese", "Other"]
-    },
-    {
-        text: "What's your favorite drink?",
-        choices: ["Coffee", "Tea", "Juice", "Milk", "Water", "Other"]
-    }
-    // Add 10 more questions here
-  ];
 
   const handleAnswer = (answer) => {
     setProgress(progress + 1);
     setAnswers([...answers.slice(0, progress), answer]);
     const radios = document.getElementsByName('answer');
     for (let i = 0; i < radios.length; i++) {
-        radios[i].checked = false;
-
+      radios[i].checked = false;
     }
   };
 
@@ -44,41 +22,44 @@ const RoommatePairing = ({ hidden }) => {
 
   const currentQuestion = questions[progress];
   return (
-     <div hidden={hidden}>
-  <div className="row">
-    <div className="col-lg-8 offset-lg-2">
-      <div className="progress mt-5 mb-5 bg-light">
-        <div
-          className="progress-bar bg-primary"
-          role="progressbar"
-          style={{ width: `${(progress / questions.length) * 100}%` }}
-          aria-valuenow={progress}
-          aria-valuemin="0"
-          aria-valuemax={questions.length}
-        ></div>
+    <div hidden={hidden}>
+      <div className="row">
+        <div className="col-lg-8 offset-lg-2">
+          <div className="progress mt-5 mb-5 bg-light">
+            <div
+              className="progress-bar bg-primary"
+              role="progressbar"
+              style={{ width: `${(progress / questions.length) * 100}%` }}
+              aria-valuenow={progress}
+              aria-valuemin="0"
+              aria-valuemax={questions.length}
+            ></div>
+          </div>
+          {currentQuestion && (
+            <Form
+              className="mt-5 mb-5 text-white"
+              style={{ backgroundColor: 'rgba(33, 37, 41, 0.65)' }}
+            >
+              <Form.Group>
+                <Form.Label className="h5 m-0">{currentQuestion.question}</Form.Label>
+                {currentQuestion.answer.map((choice) => (
+                  <Form.Check
+                    type="radio"
+                    label={choice}
+                    name="answer"
+                    onChange={() => handleAnswer(choice)}
+                    className="h5 m-2 my-3"
+                  />
+                ))}
+              </Form.Group>
+            </Form>
+          )}
+          <Button onClick={handleRedo} variant="primary" size="lg" className="mt-3">
+            Redo
+          </Button>
+        </div>
       </div>
-      {currentQuestion && (
-        <Form className="mt-5 mb-5 text-white" style={{ backgroundColor: 'rgba(33, 37, 41, 0.65)'}}>
-          <Form.Group >
-            <Form.Label className="h5 m-0">{currentQuestion.text}</Form.Label>
-            {currentQuestion.choices.map((choice) => (
-              <Form.Check
-                type="radio"
-                label={choice}
-                name="answer"
-                onChange={() => handleAnswer(choice)}
-                className="h5 m-2 my-3"
-              />
-            ))}
-          </Form.Group>
-        </Form>
-      )}
-      <Button onClick={handleRedo} variant="primary" size="lg" className="mt-3">
-        Redo
-      </Button>
     </div>
-  </div>
-</div>
   );
 };
 
