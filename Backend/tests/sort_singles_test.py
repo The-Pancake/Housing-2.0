bc568ba0f47f2ca31fa44e7194bd7c6a5ff7c47f
Daniel-Hee <danielzh334@gmail.com>
2023-10-03 17:36:38 -0400

    Basic Tests Done

--- Backend/tests/sort_singles_test.py
+++ Backend/tests/sort_singles_test.py
@@ -1,3 +1,21 @@
+#   _   _   _   _   _   _   _   _   _   _  
+#  / \ / \ / \ / \ / \ / \ / \ / \ / \ / \ 
+# ( H | o | u | s | i | n | g | 2 | . | 0 )
+#  \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ 
+#
+
+# Original Author: Daniel He 
+# This python file here is a testing file used to test sortSingles.py
+# I've set up tests in a way where anyone can run the same tests and tried to
+# make it as flexible as possible where anyone can create their own test cases
+# to test. I would highly recommend using VSCode for these tests as it has 
+# support for python unittest 
+
+# October 3rd, 2023 status - Daniel
+# Improvements need to be done with the test cases, currently they are too small scale.
+# However overall, base line test cases are finished. 
+# Next goal should be adding more realistic and detailed cases as well as edge cases
+
 import unittest
 import json
 import os
@@ -10,11 +28,11 @@ sys.path.append(dir)
 import sortSingles
 from unittest.mock import patch 
 
-
-
+# Here is were all the testing begins 
 class sortSinglesTest(unittest.TestCase):
-
-    # Set up Class, this runs once before all tests start
+    
+    # Set up Class, this runs once before all tests start, it's essentially creating
+    # attributes that all other test fuctions can access 
     @classmethod
     def setUpClass(cls):
         # Opening the test json file -> From Jack
@@ -36,7 +54,8 @@ class sortSinglesTest(unittest.TestCase):
         }
 
     # ----------------------------------------------------------------------------------
-    # Set up and tear down functions. Set up runs before each test case
+    # Set up and tear down functions. Set up runs before each test case. 
+    # These do nothing for now. 
     def setUp(self):
         pass
 
@@ -45,7 +64,6 @@ class sortSinglesTest(unittest.TestCase):
 
     # ----------------------------------------------------------------------------------
     # Test Cases
-    
     def test_weightRoom(self):
 
         print("Testing weight Room function")
@@ -62,19 +80,15 @@ class sortSinglesTest(unittest.TestCase):
 
     # For this test case function we need to use a special function called mock from the unittest
     # framework. Here patch creates a "sample" object that mimics the weightRoom function.
-    # This is because idealSearch depends on the results of weightRoom. 
+    # This is because idealSearch depends on the results of weightRoom. If we don't create this 
+    # object it can cause an error where test_WeightRoom runs fails. [ Get rid of patch to see 
+    # what I mean ;) ]
     @patch('sortSingles.weightRoom') 
     def test_idealSeach(self, mock_weightRoom):
-        
-        print("Testing ideal search function")
-        
-        # Thanks to mock, we can modify the return value of weightroom
-        mock_weightRoom.return_value = 6
-        mock_weightRoom.student = self.student
 
         # Giving a sample potential rooms for the function to use
         sampleRooms = [
-        { "Warren1-8": {
+        {
             "name": "Warren",
             "sex": "m",
             "size": 2,
@@ -103,10 +117,9 @@ class sortSinglesTest(unittest.TestCase):
                 }
             ],
             "ra room": False
-        }   
+             
         },
         {
-            "Warren1-16": {
             "name": "Warren",
             "sex": "m",
             "size": 1,
@@ -114,28 +127,50 @@ class sortSinglesTest(unittest.TestCase):
             "year": "freshman",
             "occupants": [],
             "ra room": True
-            }
         }
         ]
-        
-        sortSingles.potentialRooms.append(sampleRooms)
-        
-        # See if student is placed in the correct spot
+
+        print(sampleRooms[0])
+
+        # Thanks to mock, we can modify the return value of weightroom
+        mock_weightRoom.return_value = 6
+        mock_weightRoom.student = self.student
+
+        # Fill in potential rooms since it is being used by idealSearch :p
+        for i in range(0, len(sampleRooms)):
+            sortSingles.potentialRooms.append(sampleRooms[0])
+
+        # Where the student is expected to go
         expectedOccupants = self.dataTestFile["Warren1-8"]["occupants"]
         expectedOccupants.append(self.student)
-
         
-        # Call the function     
-        sortSingles.idealSearch(self.student)
+        # Call the function, store into result to be used for comparison later    
+        result = sortSingles.idealSearch(self.student)
         
-        self.assertEqual(expectedOccupants, self.dataTestFile["Warren1-8"]["occupants"])
+        # See if student is placed in the correct spot
+        self.assertEqual(expectedOccupants, result["occupants"] )
 
-
+    
     def test_isPerfectMatch(self):
-        self.assertEqual(1 , 1)
 
-    def test_firstSerach(self):
-        self.assertEqual(1 , 1)
+        room = self.dataTestFile["Warren1-8"]
+
+        result = sortSingles.isPerfectMatch( room, self.student )
+
+        # Perfect match should return true with current room
+        self.assertEqual(True, result)
+
+        # Next test, Test with room that doesn't match
+
+    # Test 
+    def test_firstSerach(self): 
+        
+        result = sortSingles.firstSearch( self.dataTestFile, self.student) 
+        # firstSearch should return true using current all available rooms 
+        self.assertEqual(True, result)
+
+        # Now what happens when all rooms are full? .... 
+        # Is the student being placed where we expect him to be? ... 
 
     # ----------------------------------------------------------------------------------
     # Tear down class, this runs once after all tests are over
@@ -143,7 +178,7 @@ class sortSinglesTest(unittest.TestCase):
     def tearDownClass(cls):
         cls.testFile.close() # Close test file that was opened by setupclass
 
-
+# Call unittest in main
 if __name__ == '__main__':
 
     unittest.main()
