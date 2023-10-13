b045dabdbfe3c5b2198a187fa18c012dd0859f12
rrthumma29 <rrthumma29@gmail.com>
2023-10-13 18:20:35 -0400

    Made slight changes to work better with the my best_near_building_search algorithm

--- Backend/best_near_building_search_2.0.py
+++ Backend/best_near_building_search_2.0.py
@@ -1,27 +1,83 @@
-def find_nearest_buildings(data, group1_pref, group2_pref):
-    # Initialize the nearest building pair to None and set the minimum distance to infinity.
+import json
+from IdealSearch import idealSearch
+
+dorms = ['Barton',	'Bray',	'BarH',	'Cary',	'Crockett',	'Davison',	'E_Complex',	'Hall',	'Nason',	'North',	'Quad',	'Sharp',	'Warren',	'Rahps_B',	'Blitman',	'Bryckwyck',	'Colonie',	'Rahps_A',	'Stacwyck',	'City_Station_West',	'City_Station_South',	'Polytechnic']
+
+Barton_Weights = {	          'Barton' : 0,	'Bray' : 1,	'BarH' : 3,	'Cary' : 1,	'Crockett' : 1,	'Davison' : 1,	'E-Complex' : 2,	'Hall' : 1,	'Nason' : 1,	'North' : 2,	'Quad' : 2,	'Sharp' : 2,	'Warren' : 1,	'Beman and Brinsmade (Rahps B)' : 5,	'Blitman' : 5,	'Bryckwyck' : 6,	'Colonie' : 5,	'Colvin and Albright (Rahps A)' : 3,	'Stacwyck' : 5,	'City Station West' : 6,	'City Station South' : 6,	'Polytechnic' : 5}
+Bray_Weights = {	          'Barton' : 1,	'Bray' : 0,	'BarH' : 2,	'Cary' : 1,	'Crockett' : 1,	'Davison' : 1,	'E-Complex' : 3,	'Hall' : 1,	'Nason' : 1,	'North' : 3,	'Quad' : 2,	'Sharp' : 1,	'Warren' : 1,	'Beman and Brinsmade (Rahps B)' : 5,	'Blitman' : 5,	'Bryckwyck' : 6,	'Colonie' : 5,	'Colvin and Albright (Rahps A)' : 3,	'Stacwyck' : 5,	'City Station West' : 6,	'City Station South' : 6,	'Polytechnic' : 5}
+BarH_Weights = {	          'Barton' : 3,	'Bray' : 2,	'BarH' : 0,	'Cary' : 3,	'Crockett' : 3,	'Davison' : 3,	'E-Complex' : 3,	'Hall' : 3,	'Nason' : 3,	'North' : 3,	'Quad' : 3,	'Sharp' : 3,	'Warren' : 3,	'Beman and Brinsmade (Rahps B)' : 3,	'Blitman' : 6,	'Bryckwyck' : 4,	'Colonie' : 3,	'Colvin and Albright (Rahps A)' : 1,	'Stacwyck' : 3,	'City Station West' : 8,	'City Station South' : 8,	'Polytechnic' : 6}
+Cary_Weights = {	          'Barton' : 1,	'Bray' : 1,	'BarH' : 3,	'Cary' : 0,	'Crockett' : 1,	'Davison' : 1,	'E-Complex' : 3,	'Hall' : 1,	'Nason' : 1,	'North' : 3,	'Quad' : 2,	'Sharp' : 1,	'Warren' : 1,	'Beman and Brinsmade (Rahps B)' : 5,	'Blitman' : 5,	'Bryckwyck' : 6,	'Colonie' : 5,	'Colvin and Albright (Rahps A)' : 3,	'Stacwyck' : 5,	'City Station West' : 6,	'City Station South' : 6,	'Polytechnic' : 5}
+Crockett_Weights = {          'Barton' : 1,	'Bray' : 1,	'BarH' : 3,	'Cary' : 1,	'Crockett' : 0,	'Davison' : 1,	'E-Complex' : 3,	'Hall' : 1,	'Nason' : 1,	'North' : 3,	'Quad' : 2,	'Sharp' : 1,	'Warren' : 1,	'Beman and Brinsmade (Rahps B)' : 6,	'Blitman' : 5,	'Bryckwyck' : 6,	'Colonie' : 5,	'Colvin and Albright (Rahps A)' : 3,	'Stacwyck' : 5,	'City Station West' : 6,	'City Station South' : 6,	'Polytechnic' : 5}
+Davison_Weights = {	          'Barton' : 1,	'Bray' : 1,	'BarH' : 3,	'Cary' : 1,	'Crockett' : 1,	'Davison' : 0,	'E-Complex' : 3,	'Hall' : 1,	'Nason' : 1,	'North' : 3,	'Quad' : 2,	'Sharp' : 1,	'Warren' : 1,	'Beman and Brinsmade (Rahps B)' : 6,	'Blitman' : 5,	'Bryckwyck' : 6,	'Colonie' : 5,	'Colvin and Albright (Rahps A)' : 3,	'Stacwyck' : 5,	'City Station West' : 5,	'City Station South' : 5,	'Polytechnic' : 5}
+E_Complex_Weights = {         'Barton' : 2,	'Bray' : 3,	'BarH' : 3,	'Cary' : 3,	'Crockett' : 3,	'Davison' : 3,	'E-Complex' : 0,	'Hall' : 3,	'Nason' : 3,	'North' : 3,	'Quad' : 1,	'Sharp' : 3,	'Warren' : 3,	'Beman and Brinsmade (Rahps B)' : 6,	'Blitman' : 3,	'Bryckwyck' : 6,	'Colonie' : 6,	'Colvin and Albright (Rahps A)' : 4,	'Stacwyck' : 6,	'City Station West' : 5,	'City Station South' : 5,	'Polytechnic' : 5}
+Hall_Weights = {	          'Barton' : 1,	'Bray' : 1,	'BarH' : 3,	'Cary' : 1,	'Crockett' : 1,	'Davison' : 1,	'E-Complex' : 3,	'Hall' : 0,	'Nason' : 1,	'North' : 3,	'Quad' : 2,	'Sharp' : 1,	'Warren' : 1,	'Beman and Brinsmade (Rahps B)' : 6,	'Blitman' : 5,	'Bryckwyck' : 6,	'Colonie' : 5,	'Colvin and Albright (Rahps A)' : 3,	'Stacwyck' : 5,	'City Station West' : 6,	'City Station South' : 6,	'Polytechnic' : 5}
+Nason_Weights = {	          'Barton' : 1,	'Bray' : 1,	'BarH' : 3,	'Cary' : 1,	'Crockett' : 1,	'Davison' : 1,	'E-Complex' : 3,	'Hall' : 1,	'Nason' : 0,	'North' : 3,	'Quad' : 2,	'Sharp' : 1,	'Warren' : 1,	'Beman and Brinsmade (Rahps B)' : 6,	'Blitman' : 5,	'Bryckwyck' : 6,	'Colonie' : 5,	'Colvin and Albright (Rahps A)' : 3,	'Stacwyck' : 5,	'City Station West' : 6,	'City Station South' : 6,	'Polytechnic' : 5}
+North_Weights = {	          'Barton' : 2,	'Bray' : 3,	'BarH' : 3,	'Cary' : 3,	'Crockett' : 3,	'Davison' : 3,	'E-Complex' : 3,	'Hall' : 3,	'Nason' : 3,	'North' : 0,	'Quad' : 1,	'Sharp' : 3,	'Warren' : 3,	'Beman and Brinsmade (Rahps B)' : 6,	'Blitman' : 3,	'Bryckwyck' : 6,	'Colonie' : 6,	'Colvin and Albright (Rahps A)' : 4,	'Stacwyck' : 6,	'City Station West' : 4,	'City Station South' : 4,	'Polytechnic' : 5}
+Quad_Weights = {	          'Barton' : 2,	'Bray' : 2,	'BarH' : 3,	'Cary' : 2,	'Crockett' : 2,	'Davison' : 2,	'E-Complex' : 1,	'Hall' : 2,	'Nason' : 2,	'North' : 1,	'Quad' : 0,	'Sharp' : 2,	'Warren' : 2,	'Beman and Brinsmade (Rahps B)' : 6,	'Blitman' : 4,	'Bryckwyck' : 6,	'Colonie' : 5,	'Colvin and Albright (Rahps A)' : 3,	'Stacwyck' : 5,	'City Station West' : 5,	'City Station South' : 5,	'Polytechnic' : 4}
+Sharp_Weights = {	          'Barton' : 2,	'Bray' : 1,	'BarH' : 3,	'Cary' : 1,	'Crockett' : 1,	'Davison' : 1,	'E-Complex' : 3,	'Hall' : 1,	'Nason' : 1,	'North' : 3,	'Quad' : 2,	'Sharp' : 0,	'Warren' : 1,	'Beman and Brinsmade (Rahps B)' : 6,	'Blitman' : 5,	'Bryckwyck' : 6,	'Colonie' : 5,	'Colvin and Albright (Rahps A)' : 4,	'Stacwyck' : 6,	'City Station West' : 5,	'City Station South' : 5,	'Polytechnic' : 4}
+Warren_Weights = {	          'Barton' : 1,	'Bray' : 1,	'BarH' : 3,	'Cary' : 1,	'Crockett' : 1,	'Davison' : 1,	'E-Complex' : 3,	'Hall' : 1,	'Nason' : 1,	'North' : 3,	'Quad' : 2,	'Sharp' : 1,	'Warren' : 0,	'Beman and Brinsmade (Rahps B)' : 6,	'Blitman' : 5,	'Bryckwyck' : 6,	'Colonie' : 5,	'Colvin and Albright (Rahps A)' : 3,	'Stacwyck' : 5,	'City Station West' : 6,	'City Station South' : 6,	'Polytechnic' : 5}
+Rahps_B_Weights = {        	  'Barton' : 5,	'Bray' : 5,	'BarH' : 3,	'Cary' : 5,	'Crockett' : 6,	'Davison' : 6,	'E-Complex' : 6,	'Hall' : 6,	'Nason' : 6,	'North' : 6,	'Quad' : 6,	'Sharp' : 6,	'Warren' : 6,	'Beman and Brinsmade (Rahps B)' : 0,	'Blitman' : 9,	'Bryckwyck' : 1,	'Colonie' : 3,	'Colvin and Albright (Rahps A)' : 3,	'Stacwyck' : 1,	'City Station West' : 10,	'City Station South' : 10,	'Polytechnic' : 9}
+Blitman_Weights = {	          'Barton' : 5,	'Bray' : 5,	'BarH' : 6,	'Cary' : 5,	'Crockett' : 5,	'Davison' : 5,	'E-Complex' : 3,	'Hall' : 5,	'Nason' : 5,	'North' : 3,	'Quad' : 4,	'Sharp' : 5,	'Warren' : 5,	'Beman and Brinsmade (Rahps B)' : 9,	'Blitman' : 0,	'Bryckwyck' : 9,	'Colonie' : 8,	'Colvin and Albright (Rahps A)' : 6,	'Stacwyck' : 8,	'City Station West' : 3,	'City Station South' : 3,	'Polytechnic' : 6}
+Bryckwyck_Weights = {         'Barton' : 6,	'Bray' : 6,	'BarH' : 4,	'Cary' : 6,	'Crockett' : 6,	'Davison' : 6,	'E-Complex' : 6,	'Hall' : 6,	'Nason' : 6,	'North' : 6,	'Quad' : 6,	'Sharp' : 6,	'Warren' : 6,	'Beman and Brinsmade (Rahps B)' : 1,	'Blitman' : 9,	'Bryckwyck' : 0,	'Colonie' : 3,	'Colvin and Albright (Rahps A)' : 3,	'Stacwyck' : 1,	'City Station West' : 10,	'City Station South' : 10,	'Polytechnic' : 10}
+Colonie_Weights = {	          'Barton' : 5,	'Bray' : 5,	'BarH' : 3,	'Cary' : 5,	'Crockett' : 5,	'Davison' : 5,	'E-Complex' : 6,	'Hall' : 5,	'Nason' : 5,	'North' : 6,	'Quad' : 5,	'Sharp' : 5,	'Warren' : 5,	'Beman and Brinsmade (Rahps B)' : 3,	'Blitman' : 8,	'Bryckwyck' : 3,	'Colonie' : 0,	'Colvin and Albright (Rahps A)' : 3,	'Stacwyck' : 3,	'City Station West' : 9,	'City Station South' : 9,	'Polytechnic' : 9}
+Rahps_A_Weights = {        	  'Barton' : 3,	'Bray' : 3,	'BarH' : 1,	'Cary' : 3,	'Crockett' : 3,	'Davison' : 3,	'E-Complex' : 4,	'Hall' : 3,	'Nason' : 3,	'North' : 4,	'Quad' : 3,	'Sharp' : 4,	'Warren' : 3,	'Beman and Brinsmade (Rahps B)' : 3,	'Blitman' : 6,	'Bryckwyck' : 3,	'Colonie' : 3,	'Colvin and Albright (Rahps A)' : 0,	'Stacwyck' : 3,	'City Station West' : 8,	'City Station South' : 8,	'Polytechnic' : 8}
+Stacwyck_Weights = {	      'Barton' : 5,	'Bray' : 5,	'BarH' : 3,	'Cary' : 5,	'Crockett' : 5,	'Davison' : 5,	'E-Complex' : 6,	'Hall' : 5,	'Nason' : 5,	'North' : 6,	'Quad' : 5,	'Sharp' : 6,	'Warren' : 5,	'Beman and Brinsmade (Rahps B)' : 1,	'Blitman' : 8,	'Bryckwyck' : 1,	'Colonie' : 3,	'Colvin and Albright (Rahps A)' : 3,	'Stacwyck' : 0,	'City Station West' : 10,	'City Station South' : 10,	'Polytechnic' : 9}
+City_Station_West_Weights = { 'Barton' : 6,	'Bray' : 6,	'BarH' : 8,	'Cary' : 6,	'Crockett' : 6,	'Davison' : 5,	'E-Complex' : 5,	'Hall' : 6,	'Nason' : 6,	'North' : 4,	'Quad' : 5,	'Sharp' : 5,	'Warren' : 6,	'Beman and Brinsmade (Rahps B)' : 10,	'Blitman' : 3,	'Bryckwyck' : 10,	'Colonie' : 9,	'Colvin and Albright (Rahps A)' : 8,	'Stacwyck' : 10,'City Station West' : 0,	'City Station South' : 1,	'Polytechnic' : 5}
+City_Station_South_Weights = {'Barton' : 6,	'Bray' : 6,	'BarH' : 8,	'Cary' : 6,	'Crockett' : 6,	'Davison' : 5,	'E-Complex' : 5,	'Hall' : 6,	'Nason' : 6,	'North' : 4,	'Quad' : 5,	'Sharp' : 5,	'Warren' : 6,	'Beman and Brinsmade (Rahps B)' : 10,	'Blitman' : 3,	'Bryckwyck' : 10,	'Colonie' : 9,	'Colvin and Albright (Rahps A)' : 8,	'Stacwyck' : 10,'City Station West' : 1,	'City Station South' : 0,	'Polytechnic' : 5}
+Polytechnic_Weights = {       'Barton' : 5,	'Bray' : 5,	'BarH' : 6,	'Cary' : 5,	'Crockett' : 5,	'Davison' : 5,	'E-Complex' : 5,	'Hall' : 5,	'Nason' : 5,	'North' : 5,	'Quad' : 4,	'Sharp' : 4,	'Warren' : 5,	'Beman and Brinsmade (Rahps B)' : 9,	'Blitman' : 6,	'Bryckwyck' : 10,	'Colonie' : 9,	'Colvin and Albright (Rahps A)' : 8,	'Stacwyck' : 9,	'City Station West' : 5,	'City Station South' : 5,	'Polytechnic' : 0}
+
+# Combine all the individual dictionaries into one big dictionary
+all_dorm_weights = {
+    'Barton': Barton_Weights,
+    'Bray': Bray_Weights,
+    'BarH': BarH_Weights,
+    'Cary': Cary_Weights,
+    'Crockett': Crockett_Weights, 
+    'Davison': Davison_Weights,
+    'EComplex': E_Complex_Weights,
+    'Hall': Hall_Weights,
+    'Nason': Nason_Weights,
+    'North': North_Weights,
+    'Quad': Quad_Weights,
+    'Sharp': Sharp_Weights,
+    'Warren': Warren_Weights,   
+    'RahpsB': Rahps_B_Weights, 
+    'Blitman': Blitman_Weights,
+    'Bryckwyck': Bryckwyck_Weights,
+    'Colonie': Colonie_Weights,
+    'RahpsA': Rahps_A_Weights,
+    'Stackwyck': Stacwyck_Weights,
+    'CityStationWest': City_Station_West_Weights,
+    'CityStationSouth': City_Station_South_Weights,
+    'Polytechnic': Polytechnic_Weights,
+}
+
+def Dist_Rating(dorm1, dorm2):
+    return all_dorm_weights[dorm1][dorm2]
+
+def find_nearest_available_buildings(data, group1_pref, group2_pref, group1, group2):
     nearest_building_pair = None
     min_distance = float('inf')
     
-    # Iterate through each building preference of group 1
     for building1 in group1_pref:
-        # Iterate through each building preference of group 2
         for building2 in group2_pref:
-            # Create weight keys based on the building names
-            weight_key1 = building1 + "_Weights"
-            weight_key2 = building2 + "_Weights"
-            
-            # Check if the weight keys exist in global variables
-            if weight_key1 in globals() and weight_key2 in globals():
-                # Calculate the relative distance between the two buildings using a function named Dist_Rating
-                # The function presumably takes in two arguments: weight and building name
-                distance = Dist_Rating(globals()[weight_key1], building2) + Dist_Rating(globals()[weight_key2], building1)
-                
-                # Update the nearest building pair if this distance is shorter than the previously recorded minimum distance
+            if idealSearch(data, len(group1), [building1], group1) and idealSearch(data, len(group2), [building2], group2):
+                distance = Dist_Rating(building1, building2)
                 if distance < min_distance:
                     min_distance = distance
                     nearest_building_pair = (building1, building2)
 
-    # Return the nearest building pair
     return nearest_building_pair
+
+filename = "Backend/Campus_data_structures/campus4.json"
+with open(filename) as f:
+    data = json.load(f)
+
+group1 = ['kellie', 'Becky']
+group2 = ['John', 'Doe']
+group1_pref = ["Davison", "Barton", "Bray"]
+group2_pref = ["Sharp", "BarH", "Cary"]
+
+result = find_nearest_available_buildings(data, group1_pref, group2_pref, group1, group2)
+print(f"Nearest Buildings with available rooms for the groups are: {result}")
 
--- Backend/idealSearch.py
+++ Backend/idealSearch.py
@@ -1,87 +1,58 @@
-# Implement Algorithm Here:
 import json
-# -ideal_search: Finds a room with a size that matches the group size and takes preferences into account
 
-def idealSearch(name, data, groupSize, preferred):
-    # check to see if groupSize matches the room size available
-    # preferred is a list of their preferred dorms in order from first to last, can be up to 3 <= x <= 5 dorms
-    # returns a string of the best dorm available for them
-    # data is read from a json file
-    # if group size is alr 4, just get them a room
-    given_dorm = ""
-    room = ""
-    if groupSize == 2 or groupSize == 4:
-        # if groupsize is 2 for a double (basically fresh housing or quad)
-        # what it needs to do: add one person to a room w one person
-        first_choice = preferred[0]
-        hall = data[first_choice]
-        count = 0
-        for i in hall:
-            room_one = hall[i]
-            occ = room_one["Occupants"]
-            size_ = room_one["size"]
-            if len(occ) == 0:
-                for x in name:
-                    occ.append(x)
-                count += 1
-                room = first_choice
-                break
-        if count == 0:
-            next_choice = preferred[1]
-            room = next_choice
-            hall = data[next_choice]
-            for i in hall:
-                room_one = hall[i]
-                occ = room_one["Occupants"]
-                size_ = room_one["size"]
-                if len(occ) < size_:
-                    for x in name:
-                        occ.append(x)
-                    count+=1
-                    break
-    elif groupSize == 1:
-        first_choice = preferred[0]
-        hall = data[first_choice]
-        count = 0
-        for i in hall:
-            room_one = hall[i]
-            occ = room_one["Occupants"]
-            size_ = room_one["size"]
-            if len(occ) == 1:
-                for x in name:
-                    occ.append(x)
-                count += 1
-                room = first_choice
-                break
-    
-    given_dorm = room + " " + room_one["id"]
+def idealSearch(data, groupSize, preferred, students):
+    for pref in preferred:
+        hall = data.get(pref, {}).get("Rooms", {}) 
+        for room_id, room_data in hall.items():
+            occupants = room_data["Occupants"]
+            size = room_data["size"]
+            if len(occupants) + groupSize <= size:
+                for student in students:
+                    occupants.append(student)
+                room_data["Occupants"] = occupants
+                return True, pref + " " + room_id  
+            elif len(occupants) == 0 and groupSize <= size:
+                for student in students[:size]:
+                    occupants.append(student)
+                room_data["Occupants"] = occupants
+                return True, pref + " " + room_id  
+    return False, None
 
-    return data, given_dorm
+
+def updateJSON(filename, data):
+    with open(filename, 'w') as f:
+        json.dump(data, f, indent=4)
 
 def printData(data):
-    for i in data:
-        print(i, ":")
-        data_set = data[i]
-        for j in data_set:
-            print("  ", j, ":")
-            data_s = data_set[j]
-            id_ = data_s["id"]
-            size = data_s["size"]
-            shared_bath = data_s["shared_bathroom"]
-            type_ = data_s["type"]
-            occupants = data_s["Occupants"]
-            print("    room number:", id_, "\n    room size:", size)
-            print("    shared bathroom:", shared_bath, "\n    type:", type_)
+    for hall, rooms in data.items():
+        print(hall, ":")
+        for room_id, room_data in rooms.items():
+            print("  ", room_id, ":")
+            print("    room size:", room_data["size"])
+            print("    shared bathroom:", room_data["shared_bathroom"])
+            print("    type:", room_data["type"])
             print("    occupants:")
-            for x in occupants:
-                print("\t", x)
+            for occupant in room_data["Occupants"]:
+                print("\t", occupant)
 
-f = open("test.json")
-data = json.load(f)
-preferred1 = ["Warren", "Nason", "Davison"]
-preferred2 = ["Sharp", "Hall"]
-name = ["kellie"]
-groupSize = len(name)
-data, given_room = idealSearch(name, data, groupSize, preferred1)
+# Load the JSON data
+filename = "Backend/Campus_data_structures/campus3.json"
+with open(filename) as f:
+    data = json.load(f)
+
+preferred1 = ["Davison"]
+preferred2 = ["Sharp"]
+students = ["kellie", "Becky"]
+groupSize = len(students)
+
+room_found, given_room = idealSearch(data, groupSize, preferred1, students)
+
+if room_found:
+    updateJSON(filename, data)
+    print("Room found:", given_room)
+else:
+    print("No suitable room found for the group.")
+    
 printData(data)
-print(given_room)
+
+
