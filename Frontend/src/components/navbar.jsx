9894e9288e06556a41ab14ca19c206f58f40ba33
James Liu <liuxjames88@gmail.com>
2023-10-17 17:12:23 -0400

    fixing imports and updating package.json

--- Frontend/src/components/navbar.jsx
+++ Frontend/src/components/navbar.jsx
@@ -1,49 +1,49 @@
-import { Link } from "react-router-dom";
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
-import "./navbar.css"
+import { Link } from 'react-router-dom';
+import { LinkContainer } from 'react-router-bootstrap';
 
-export default function Navbar() {
-  const routes = [
-    {
-      path: "",
-      name: "index"
-    },
-    {
-      path: "dorms",
-      name: "dorms"
-    },
-    {
-      path: "account",
-      name: "account info"
-    },
-    {
-      path: "contact",
-      name: "contact us"
-    },
-    {
-      path: "application",
-      name: "application status"
-    },
-    {
-      path: "signup",
-      name: "signup page"
-    }
-  ];
-
-  return (
-    <ul id="navbar">
-      <li className="logo">
-        <Link to="">
-          <h1>Housing 2.0</h1>
-        </Link>
-      </li>
-      {routes.map((route) => {
-        return (
-          <li key={route.path}>
-            <Link to={route.path}>{route.name}</Link>
-          </li>
-        );
-      })}
-    </ul>
-  );
+const NavbarLink = () => {
+    return (
+        <Navbar expand="lg" className="bg-body-tertiary" fixed="top" style={{ width: '100vw' }}>
+            <Container fluid>
+                <Navbar.Brand href="/">Housing 2.0</Navbar.Brand>
+                <Navbar.Toggle aria-controls="navbarScroll" />
+                <Navbar.Collapse id="navbarScroll">
+                    <Nav
+                        className="me-auto my-2 my-lg-0"
+                        style={{ maxHeight: '100px' }}
+                        navbarScroll
+                    >
+                        <Nav.Link href="#action1">Home</Nav.Link>
+                        <Nav.Link href="#action2"><Link to="/">Link</Link></Nav.Link>
+                        <NavDropdown title="Link" id="navbarScrollingDropdown">
+                            <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action4">
+                                Another action
+                            </NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action5">
+                                Something else here
+                            </NavDropdown.Item>
+                        </NavDropdown>
+                        <Nav.Link href="#" disabled>
+                            Link
+                        </Nav.Link>
+                    </Nav>
+                    <Form className="d-flex">
+                        <Button variant="outline-success"><Link to="/login">Login</Link></Button>
+                        <Button variant="outline-success"><Link to="/signup">Signup</Link></Button>
+                    </Form>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
 }
+
+export default NavbarLink;
