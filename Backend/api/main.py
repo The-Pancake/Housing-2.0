6e5c4253ae1dc109bb4db9348b6708505456718b
James Liu <liuxjames88@gmail.com>
2023-10-17 16:44:54 -0400

    changing tabes from 2 to 4 space

--- Backend/api/main.py
+++ Backend/api/main.py
@@ -5,75 +5,72 @@ import uvicorn
 import json
 
 
-
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
-  return {"message": "Hello World"}
+    return {"message": "Hello World"}
 
 
 @app.get("/profile")
 async def profile(res: Request):
-  # is user signed in?
-  if (not res.cookies.get("account")):
-    return False
+    # is user signed in?
+    if (not res.cookies.get("account")):
+        return False
 
-
-  print(res.cookies.get("account"))
-  return {"message": "tbd"}
+    print(res.cookies.get("account"))
+    return {"message": "tbd"}
 
 
 @app.post("/signin")
 async def signin(req: Request, res: Response):
-  # todo: add error checking
-  reqJson = await req.json()
-  email = reqJson.get("email")
-  password = reqJson.get("password")
-  if (not email or not password):
-    res.status_code = 400
-    return {"message": "Error: missing email or password"}
+    # todo: add error checking
+    reqJson = await req.json()
+    email = reqJson.get("email")
+    password = reqJson.get("password")
+    if (not email or not password):
+        res.status_code = 400
+        return {"message": "Error: missing email or password"}
 
-  loginFile = open("./data/logins.json", "r")
-  loginInfo = json.load(loginFile)
+    loginFile = open("./data/logins.json", "r")
+    loginInfo = json.load(loginFile)
 
-  if (loginInfo.get(email) == password):
-    res.set_cookie(key="account", value=email)
-    return True
-  return False
+    if (loginInfo.get(email) == password):
+        res.set_cookie(key="account", value=email)
+        return True
+    return False
+
 
 @app.post("/signup")
 async def signup(req: Request, res: Response):
-  # todo: add error checking here
-  # also maybe try to store just the password hashes
-  #  instead of the passwords in plaintext
+    # todo: add error checking here
+    # also maybe try to store just the password hashes
+    #  instead of the passwords in plaintext
 
-  # validate that there is email and password present
-  #  in the request body
-  reqJson = await req.json()
-  email = reqJson.get("email")
-  password = reqJson.get("password")
-  if (not email or not password):
-    res.status_code = 400
-    return {"message": "Error: missing email or password"}
+    # validate that there is email and password present
+    #  in the request body
+    reqJson = await req.json()
+    email = reqJson.get("email")
+    password = reqJson.get("password")
+    if (not email or not password):
+        res.status_code = 400
+        return {"message": "Error: missing email or password"}
 
-  loginFile = open("./data/logins.json", "r")
-  loginInfo = json.load(loginFile)
-  
-  loginInfo[email] = password
-  loginFile.close()
-  loginFile = open("./data/logins.json", "w")
-  
+    loginFile = open("./data/logins.json", "r")
+    loginInfo = json.load(loginFile)
 
-  json.dump(loginInfo, loginFile)
+    loginInfo[email] = password
+    loginFile.close()
+    loginFile = open("./data/logins.json", "w")
 
-  loginFile.close()
-  res.status_code = 200
-  return {"message": f"Successfully created new account {email} {password}"}
+    json.dump(loginInfo, loginFile)
 
-  
-  
+    loginFile.close()
+    res.status_code = 200
+    return {"message": f"Successfully created new account {email} {password}"}
+
 
 if __name__ == "__main__":
-  uvicorn.run("main:app", host="127.0.0.1", port=3001, log_level="info", reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=3001,
+                log_level="info", reload=True)
