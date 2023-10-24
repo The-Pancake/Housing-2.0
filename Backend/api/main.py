e27dad2372fca25b08c5b146092829e549b16071
James Liu <liuxjames88@gmail.com>
2023-10-24 16:33:15 -0400

    connecting to mongo

--- Backend/api/main.py
+++ Backend/api/main.py
@@ -3,6 +3,16 @@
 from fastapi import FastAPI, Request, Response
 import uvicorn
 import json
+from pymongo import MongoClient
+
+
+
+# ------------------ Database ------------------
+# Provide the mongodb atlas url to connect python to mongodb using pymongo
+CONNECTION_STRING = "mongodb+srv://housing20rcos:nQWnpyw4PsFk78eJ@housing2.elxx6hn.mongodb.net/?retryWrites=true&w=majority"
+# Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
+client = MongoClient(CONNECTION_STRING)
+# ----------------------------------------------
 
 
 app = FastAPI()
@@ -33,13 +43,15 @@ async def signin(req: Request, res: Response):
         res.status_code = 400
         return {"message": "Error: missing email or password"}
 
-    loginFile = open("./data/logins.json", "r")
-    loginInfo = json.load(loginFile)
 
-    if (loginInfo.get(email) == password):
-        res.set_cookie(key="account", value=email)
-        return True
-    return False
+    # loginFile = open("./data/logins.json", "r")
+    # loginInfo = json.load(loginFile)
+
+    loginInfo = client["Student_Info"]["Logins"].find_one({"email": email, "password": password})
+    if (loginInfo == None):
+        return False
+    res.set_cookie(key="account", value=email)
+    return True
 
 
 @app.post("/signup")
@@ -74,3 +86,20 @@ async def signup(req: Request, res: Response):
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=3001,
                 log_level="info", reload=True)
+
+
+
+
+
+# This is added so that many files can reuse the function get_database()
+if __name__ == "__main__":
+
+    # Get the database
+    dbname = get_database("Campus")
+    print(dbname)
+    print(dbname.list_collection_names())
+    temp = dbname["Dorms"].find()
+    print(temp)
+    print(type(temp))
+    for x in temp:
+        print(x)
