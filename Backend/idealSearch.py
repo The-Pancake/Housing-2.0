7f25492237102de6fc224df6f59281fb98cece90
rrthumma29 <rrthumma29@gmail.com>
2023-11-10 17:36:45 -0500

    Finished best_near_building_search algorithm utilizing the Idealsearch algorithm with mongoDb integrated. Removed testing from Idealsearch as it was inteferring with best_near_building_search

--- Backend/idealSearch.py
+++ Backend/idealSearch.py
@@ -5,7 +5,7 @@ def connect_to_db(uri):
     db = client['Campus'] 
     return db
 
-def idealSearch(db, groupSize, preferred, students):
+def searchIdeal(db, groupSize, preferred, students):
     for pref in preferred:
         # Query to find rooms with enough space and empty Occupants array
         room = db['Dorms_Rohan'].find_one({
@@ -23,19 +23,6 @@ def idealSearch(db, groupSize, preferred, students):
             return True, f"{pref} Room {room['RoomNum']}"  # Return the dorm name and room number
     return False, None
 
-
-def printData(db):
-    dorms = db['Dorms_Rohan'].find()
-    for dorm in dorms:
-        print(dorm["Dorm"], ":")
-        print("  ", dorm["RoomNum"], ":")
-        print("    room size:", dorm["Size"])
-        print("    shared bathroom:", dorm["sharedBathroom"])
-        print("    type:", dorm["type"])
-        print("    occupants:")
-        for occupant in dorm["Occupants"]:
-            print("\t", occupant)
-
 # Connection URI
 uri = "mongodb+srv://housing20rcos:nQWnpyw4PsFk78eJ@housing2.elxx6hn.mongodb.net/?retryWrites=true&w=majority"
 
@@ -47,16 +34,6 @@ except Exception as e:
     print(e)
     exit()
 
-preferences = ["Barton", "Hall", "Blitman", "Davison"]
-students = ["Selena", "Nicky", "Beyonce"]
-groupSize = len(students)
 
-room_found, given_room = idealSearch(db, groupSize, preferences, students)
 
-if room_found:
-    print("Room found:", given_room)
-else:
-    print("No suitable room found for the group.")
-
-printData(db)
 
