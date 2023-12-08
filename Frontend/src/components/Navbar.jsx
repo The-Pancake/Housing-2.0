635a3e8738af7e448ac866200a74cfa315061f60
Paul Gaddy <35400575+notpaulgaddy@users.noreply.github.com>
2023-12-07 19:56:35 -0500

    Navbar updates

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,6 +1,5 @@
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
@@ -10,40 +9,29 @@ import { Link } from 'react-router-dom';
 
 const NavbarLink = () => {
     return (
-        <Navbar expand="lg" className="bg-body-tertiary" fixed="top" style={{ width: '100vw' }}>
-            <Container fluid>
-                <Navbar.Brand href="/">Housing 2.0</Navbar.Brand>
-                <Navbar.Toggle aria-controls="navbarScroll" />
-                <Navbar.Collapse id="navbarScroll">
-                    <Nav
-                        className="me-auto my-2 my-lg-0"
-                        style={{ maxHeight: '100px' }}
-                        navbarScroll
-                    >
-                        <Nav.Link href="#action1">Home</Nav.Link>
-                        <Nav.Link href="#action2"><Link to="/">Link</Link></Nav.Link>
-                        <Nav.Link><Link to="/application">Profile</Link></Nav.Link> 
-                        <NavDropdown title="Link" id="navbarScrollingDropdown">
-                            <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action4">
-                                Another action
-                            </NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action5">
-                                Something else here
-                            </NavDropdown.Item>
-                        </NavDropdown>
-                        <Nav.Link href="#" disabled>
-                            Link
-                        </Nav.Link>
-                    </Nav>
-                    <Form className="d-flex">
-                        <Button variant="outline-success"><Link to="/login">Login</Link></Button>
-                        <Button variant="outline-success"><Link to="/signup">Signup</Link></Button>
-                    </Form>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
+        <Navbar expand="lg" className="bg-body-tertiary">
+      <Container fluid>
+        <Navbar.Brand href="/">Housing 2.0</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Nav.Link href="/dorms">Dorms</Nav.Link>
+            <Nav.Link href="/faq">FAQ</Nav.Link>
+            <Nav.Link href="/quiz">Quiz</Nav.Link>
+            <Nav.Link href="/application">Application</Nav.Link>
+            <Nav.Link href="/account">Account</Nav.Link>
+            <Nav.Link href="/contact">Contact</Nav.Link>
+
+          </Nav>
+          <Button variant="outline-success" href="/signup">Signup</Button>
+          <Button variant="outline-success" href="/login">Login</Button>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
     );
 }
 
