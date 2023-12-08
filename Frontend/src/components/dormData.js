2de9efc31d5ddfdbf5fa044e0983a15ac36e1cef
Aditya-Dhodapkar <68459770+Aditya-Dhodapkar@users.noreply.github.com>
2023-12-08 11:25:57 -0500

    half-changes

--- Frontend/src/components/dormData.js
+++ Frontend/src/components/dormData.js
@@ -1,79 +1,91 @@
+import bray_pic from './src/bray_hall.jpg'
+import cary_pic from './src/cary_hall.jpg'
+import barton_pic from './src/barton_hall.jpg'
+import warren_pic from './src/warren_hall.jpg'
+import hall_pic from './src/hall_hall.jpg'
+
 export default [
     {
-        imageOne: "https://www.asc-cybernetics.org/2010/wp-content/uploads/2010/07/blitman.jpg",
-        imageTwo: "https://cdn.shopify.com/s/files/1/1429/6260/products/AHTD-TOUR-MERCH-PRODUCT-SHOTS---POSTER-TEE-FRONT_800x.png?v=1663686021",
-        imageThree: "https://cdn.shopify.com/s/files/1/1429/6260/products/AHTD-TOUR-MERCH-PRODUCT-SHOTS---POSTER-TEE-FRONT_800x.png?v=1663686021",
-        imageFour: "https://cdn.shopify.com/s/files/1/1429/6260/products/AHTD-TOUR-MERCH-PRODUCT-SHOTS---POSTER-TEE-FRONT_800x.png?v=1663686021",
-        dormName: "AHTD Poster Tee",
+        //SHARP HALL
+        imageOne: "https://lh3.googleusercontent.com/p/AF1QipOD0-C34NBO0tBfnLYe5S-MyPt7UZ-AxaEjXUo9=s680-w680-h510", //changed
+        imageTwo: "https://lh3.googleusercontent.com/p/AF1QipObgJDTYrjpKTLcykOn3BdTOTM1Zs7h9OnePi05=s680-w680-h510", //changed
+        imageThree: "https://lh3.googleusercontent.com/p/AF1QipNqcihpB03Hl8s_PtQYzGjhqTsZLbYXse4tI3lI=s680-w680-h510", //changed
+        imageFour: "https://lh3.googleusercontent.com/p/AF1QipN473DlDIJA8jwqd3Dp8hxSfX9EiBRac3frls6t=s680-w680-h510", //changed
+        dormName: "Sharp Hall",
         dormPrice: "50",
         category: "shirt",
         status: "Available",
-        dormAddress: "Barton Hall, Troy, NY 12180",
-        id: "barton",
+        dormAddress: "Sharp Hall, 1999 Burdett Ave, Troy, New York, 12180",
+        id: "sharp_hall",
         rating: "5",
         numRooms: 1,
         numBeds: 2,
         sqft: 140,
         ratingAmt: 120
     }, {
-        imageOne: "https://www.asc-cybernetics.org/2010/wp-content/uploads/2010/07/blitman.jpg",
-        imageTwo: "https://cdn.shopify.com/s/files/1/1429/6260/products/AHTD-TOUR-MERCH-PRODUCT-SHOTS---POSTER-TEE-FRONT_800x.png?v=1663686021",
+        //BARTON HALL
+        imageOne: "https://lh3.googleusercontent.com/p/AF1QipM5wA0AqytzzokOERcI9WIz86-alFGdzO-SXnzy=s680-w680-h510", //changed
+        imageTwo: "https://lh3.googleusercontent.com/p/AF1QipPStpriupdNAYr6g9-X6yD7CxKmSGlQyPFqW9Xj=s680-w680-h510", //changed
         imageThree: "https://cdn.shopify.com/s/files/1/1429/6260/products/AHTD-TOUR-MERCH-PRODUCT-SHOTS---POSTER-TEE-FRONT_800x.png?v=1663686021",
         imageFour: "https://cdn.shopify.com/s/files/1/1429/6260/products/AHTD-TOUR-MERCH-PRODUCT-SHOTS---POSTER-TEE-FRONT_800x.png?v=1663686021",
-        dormName: "AHTD Tour Tee",
+        dormName: "Barton Hall",
         dormPrice: "50",
         category: "shirt",
         status: "Available",
-        dormAddress: "None",
-        id: "shirt2",
+        dormAddress: "Barton Hall, 1999 Burdett Ave, Troy, New York, 12180",
+        id: "barton_hall",
         rating: "5",
         numRooms: 1,
         numBeds: 2,
         sqft: 140,
         ratingAmt: 120
     }, {
-        imageOne: "https://www.asc-cybernetics.org/2010/wp-content/uploads/2010/07/blitman.jpg",
-        imageTwo: "https://cdn.shopify.com/s/files/1/1429/6260/products/AHTD-TOUR-MERCH-PRODUCT-SHOTS---POSTER-TEE-FRONT_800x.png?v=1663686021",
-        imageThree: "https://cdn.shopify.com/s/files/1/1429/6260/products/AHTD-TOUR-MERCH-PRODUCT-SHOTS---POSTER-TEE-FRONT_800x.png?v=1663686021",
-        imageFour: "https://cdn.shopify.com/s/files/1/1429/6260/products/AHTD-TOUR-MERCH-PRODUCT-SHOTS---POSTER-TEE-FRONT_800x.png?v=1663686021",
-        dormName: "AHTD XOHF Tee",
+        //CARY HALL
+        imageOne: "https://live.staticflickr.com/4118/4802687555_df28edf3cb_b.jpg", //changed
+        imageTwo: "https://lh3.googleusercontent.com/p/AF1QipNLP54Acp7ncHc_xnfa4bPdmsArjU61Y8xeTrM7=s680-w680-h510", //chnaged
+        imageThree: "https://rpirooms.files.wordpress.com/2014/03/hall-2.jpg", //changed
+        imageFour: cary_pic, //changed
+        dormName: "Cary Hall",
         dormPrice: "50",
         category: "shirt",
         status: "Available",
-        dormAddress: "None",
-        id: "shirt3",
+        dormAddress: "Cary Hall, 1999 Burdett Ave, Troy, New York, 12180",
+        id: "cary_hall",
         rating: "5",
         numRooms: 1,
         numBeds: 2,
         sqft: 140,
         ratingAmt: 120
     }, {
-        imageOne: "https://www.asc-cybernetics.org/2010/wp-content/uploads/2010/07/blitman.jpg",
-        imageTwo: "https://cdn.shopify.com/s/files/1/1429/6260/products/AHTD-TOUR-MERCH-PRODUCT-SHOTS---POSTER-TEE-FRONT_800x.png?v=1663686021",
-        imageThree: "https://cdn.shopify.com/s/files/1/1429/6260/products/AHTD-TOUR-MERCH-PRODUCT-SHOTS---POSTER-TEE-FRONT_800x.png?v=1663686021",
-        imageFour: "https://cdn.shopify.com/s/files/1/1429/6260/products/AHTD-TOUR-MERCH-PRODUCT-SHOTS---POSTER-TEE-FRONT_800x.png?v=1663686021",
-        dormName: "DAWN FM Tee",
+        //BLITMAN HALL
+        imageOne: "https://pocketsights.com/tours/place/Blitman-Commons-1046:214#lg=1&slide=0", //changed
+        imageTwo: "https://images.squarespace-cdn.com/content/v1/5c6d9c6a7eb88c336af0afed/1564509683417-VNV4OCA9X0PYAI3WILXI/DSC_8004.jpg?format=1000w", //changed
+        imageThree: "https://www.troyrecord.com/wp-content/uploads/migration/2009/05/99dbb633be02e870ac4f920fed71f040.jpg?w=620", //changed
+        imageFour: "https://www.troyrecord.com/wp-content/uploads/migration/2009/05/aaf2e6b39837aeabb08b2ce286a986e3.jpg?w=620", //changed
+        dormName: "Blitman",
         dormPrice: "40",
         category: "shirt",
         status: "Available",
-        dormAddress: "None",
-        id: "shirt4",
+        dormAddress: "1800 6th Ave, Troy, New York, 12180",
+        id: "blitman_hall",
         rating: "5",
         numRooms: 1,
         numBeds: 2,
         sqft: 140,
         ratingAmt: 120
     }, {
-        imageOne: "https://www.asc-cybernetics.org/2010/wp-content/uploads/2010/07/blitman.jpg",
-        imageTwo: "https://cdn.shopify.com/s/files/1/1429/6260/products/AHTD-TOUR-MERCH-PRODUCT-SHOTS---POSTER-TEE-FRONT_800x.png?v=1663686021",
+
+        //BRAY HALL
+        imageOne: bray_pic, //changed
+        imageTwo: "https://rpirooms.files.wordpress.com/2014/03/bray.jpg", //changed
         imageThree: "https://cdn.shopify.com/s/files/1/1429/6260/products/AHTD-TOUR-MERCH-PRODUCT-SHOTS---POSTER-TEE-FRONT_800x.png?v=1663686021",
         imageFour: "https://cdn.shopify.com/s/files/1/1429/6260/products/AHTD-TOUR-MERCH-PRODUCT-SHOTS---POSTER-TEE-FRONT_800x.png?v=1663686021",
-        dormName: "DAWN FM Tee",
+        dormName: "Bray Hall",
         dormPrice: "50",
         category: "shirt",
         status: "Available",
-        dormAddress: "None",
-        id: "shirt5",
+        dormAddress: "Bray Hall, 1999 Burdett Ave, Troy, New York, 12180",
+        id: "bray_hall",
         rating: "4",
         numRooms: 1,
         numBeds: 2,
@@ -81,16 +93,17 @@ export default [
         ratingAmt: 120
     },
     {
-        imageOne: "https://www.asc-cybernetics.org/2010/wp-content/uploads/2010/07/blitman.jpg",
+        //WARREN HALL
+        imageOne: warren_pic,
         imageTwo: "https://cdn.shopify.com/s/files/1/1429/6260/products/AHTD-TOUR-MERCH-PRODUCT-SHOTS---POSTER-TEE-FRONT_800x.png?v=1663686021",
         imageThree: "https://cdn.shopify.com/s/files/1/1429/6260/products/AHTD-TOUR-MERCH-PRODUCT-SHOTS---POSTER-TEE-FRONT_800x.png?v=1663686021",
         imageFour: "https://cdn.shopify.com/s/files/1/1429/6260/products/AHTD-TOUR-MERCH-PRODUCT-SHOTS---POSTER-TEE-FRONT_800x.png?v=1663686021",
-        dormName: "XO Logo Tee",
+        dormName: "Warren Hall",
         dormPrice: "40",
         category: "shirt",
         status: "Available",
         dormAddress: "None",
-        id: "shirt6",
+        id: "Warren Hall, 1999 Burdett Ave, Troy, New York, 12180",
         rating: "5",
         numRooms: 1,
         numBeds: 2,
@@ -98,16 +111,17 @@ export default [
         ratingAmt: 120
     },
     {
-        imageOne: "https://www.asc-cybernetics.org/2010/wp-content/uploads/2010/07/blitman.jpg",
-        imageTwo: "https://cdn.shopify.com/s/files/1/1429/6260/products/AHTD-TOUR-MERCH-PRODUCT-SHOTS---POSTER-TEE-FRONT_800x.png?v=1663686021",
-        imageThree: "https://cdn.shopify.com/s/files/1/1429/6260/products/AHTD-TOUR-MERCH-PRODUCT-SHOTS---POSTER-TEE-FRONT_800x.png?v=1663686021",
-        imageFour: "https://cdn.shopify.com/s/files/1/1429/6260/products/AHTD-TOUR-MERCH-PRODUCT-SHOTS---POSTER-TEE-FRONT_800x.png?v=1663686021",
-        dormName: "XO Tee",
-        dormPrice: "40",
+        //QUAD
+        imageOne: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/97/RPIQuad.jpg/700px-RPIQuad.jpg", //changed
+        imageTwo: "https://pbs.twimg.com/media/Bxwa8bGIgAAeXjV.jpg:large", //changed
+        imageThree: "https://magazine.rpi.edu/sites/default/files/styles/image_sm/public/2020-06/Student%20Highlight%20-%20Hero%20for%20Student%20Profiles.png?itok=zei4VSK2", //changed
+        imageFour: "https://giving.rpi.edu/wp-content/uploads/2017/10/IA-TVS-Quadrangle.jpg", //changed
+        dormName: "Quad",
+        dormPrice: "56",
         category: "shirt",
         status: "Available",
-        dormAddress: "None",
-        id: "shirt7",
+        dormAddress: "Quadrangle Complex, Troy, NY 12180",
+        id: "quad",
         rating: "5",
         numRooms: 1,
         numBeds: 2,
@@ -115,32 +129,34 @@ export default [
         ratingAmt: 120
     },
     {
+        //DAVISON
         imageOne: "https://www.asc-cybernetics.org/2010/wp-content/uploads/2010/07/blitman.jpg",
         imageTwo: "https://cdn.shopify.com/s/files/1/1429/6260/products/AHTD-TOUR-MERCH-PRODUCT-SHOTS---POSTER-TEE-FRONT_800x.png?v=1663686021",
         imageThree: "https://cdn.shopify.com/s/files/1/1429/6260/products/AHTD-TOUR-MERCH-PRODUCT-SHOTS---POSTER-TEE-FRONT_800x.png?v=1663686021",
         imageFour: "https://cdn.shopify.com/s/files/1/1429/6260/products/AHTD-TOUR-MERCH-PRODUCT-SHOTS---POSTER-TEE-FRONT_800x.png?v=1663686021",
-        dormName: "DAWN FM Tee",
+        dormName: "Davison Hall",
         dormPrice: "38",
         category: "shirt",
         status: "Available",
-        dormAddress: "None",
-        id: "shirt8",
+        dormAddress: "Davison Hall, 1999 Burdett Ave, Troy, New York, 12180",
+        id: "davison_hall",
         rating: "5",
         numRooms: 1,
         numBeds: 2,
         sqft: 140,
         ratingAmt: 120
     }, {
-        imageOne: "https://www.asc-cybernetics.org/2010/wp-content/uploads/2010/07/blitman.jpg",
-        imageTwo: "https://cdn.shopify.com/s/files/1/1429/6260/products/AHTD-TOUR-MERCH-PRODUCT-SHOTS---POSTER-TEE-FRONT_800x.png?v=1663686021",
+        //NASON
+        imageOne: "https://vasudha.wp.rpi.edu/files/2013/06/2013-07-09-14.47.28.jpg", //changed
+        imageTwo: "https://rpirooms.files.wordpress.com/2014/03/tumblr_kzaw239byy1qbrkqoo4_1280.jpg", //changed
         imageThree: "https://cdn.shopify.com/s/files/1/1429/6260/products/AHTD-TOUR-MERCH-PRODUCT-SHOTS---POSTER-TEE-FRONT_800x.png?v=1663686021",
         imageFour: "https://cdn.shopify.com/s/files/1/1429/6260/products/AHTD-TOUR-MERCH-PRODUCT-SHOTS---POSTER-TEE-FRONT_800x.png?v=1663686021",
-        dormName: "DAWN Cover Tee",
+        dormName: "Nason Hall",
         dormPrice: "40",
         category: "shirt",
         status: "Available",
-        dormAddress: "None",
-        id: "shirt9",
+        dormAddress: "Nason Hall, 1999 Burdett Ave, Troy, New York, 12180",
+        id: "nason_hall",
         rating: "5",
         numRooms: 1,
         numBeds: 2,
@@ -148,32 +164,35 @@ export default [
         ratingAmt: 120
     },
     {
+        //BARH
         imageOne: "https://www.asc-cybernetics.org/2010/wp-content/uploads/2010/07/blitman.jpg",
         imageTwo: "https://cdn.shopify.com/s/files/1/1429/6260/products/AHTD-TOUR-MERCH-PRODUCT-SHOTS---POSTER-TEE-FRONT_800x.png?v=1663686021",
         imageThree: "https://cdn.shopify.com/s/files/1/1429/6260/products/AHTD-TOUR-MERCH-PRODUCT-SHOTS---POSTER-TEE-FRONT_800x.png?v=1663686021",
         imageFour: "https://cdn.shopify.com/s/files/1/1429/6260/products/AHTD-TOUR-MERCH-PRODUCT-SHOTS---POSTER-TEE-FRONT_800x.png?v=1663686021",
-        dormName: "DAWN FM Tee",
+        dormName: "BARH",
         dormPrice: "38",
         category: "shirt",
         status: "Available",
-        dormAddress: "None",
-        id: "shirt10",
+        dormAddress: "212 Burdett Ave, Troy, New York, 12180",
+        id: "barh",
         rating: "5",
         numRooms: 1,
         numBeds: 2,
         sqft: 140,
         ratingAmt: 120
     }, {
+
+        //Crockett
         imageOne: "https://www.asc-cybernetics.org/2010/wp-content/uploads/2010/07/blitman.jpg",
         imageTwo: "https://cdn.shopify.com/s/files/1/1429/6260/products/AHTD-TOUR-MERCH-PRODUCT-SHOTS---POSTER-TEE-FRONT_800x.png?v=1663686021",
         imageThree: "https://cdn.shopify.com/s/files/1/1429/6260/products/AHTD-TOUR-MERCH-PRODUCT-SHOTS---POSTER-TEE-FRONT_800x.png?v=1663686021",
         imageFour: "https://cdn.shopify.com/s/files/1/1429/6260/products/AHTD-TOUR-MERCH-PRODUCT-SHOTS---POSTER-TEE-FRONT_800x.png?v=1663686021",
-        dormName: "Chrome dormName Tee",
+        dormName: "Crockett Hall",
         dormPrice: "38",
         category: "shirt",
         status: "Available",
-        dormAddress: "None",
-        id: "shirt11",
+        dormAddress: "Crockett Hall, 1999 Burdett Ave, Troy, New York, 12180",
+        id: "crocket_hall",
         rating: "5",
         numRooms: 1,
         numBeds: 2,
@@ -181,22 +200,24 @@ export default [
         ratingAmt: 120
     },
     {
-        imageOne: "https://www.asc-cybernetics.org/2010/wp-content/uploads/2010/07/blitman.jpg",
+        //NUGENT
+        imageOne: "https://lh3.googleusercontent.com/p/AF1QipMYz5eyTRQ0Da8tBPGkbNVlyTyeR-0e-uoL58bn=s680-w680-h510", //changed
         imageTwo: "https://cdn.shopify.com/s/files/1/1429/6260/products/AHTD-TOUR-MERCH-PRODUCT-SHOTS---POSTER-TEE-FRONT_800x.png?v=1663686021",
         imageThree: "https://cdn.shopify.com/s/files/1/1429/6260/products/AHTD-TOUR-MERCH-PRODUCT-SHOTS---POSTER-TEE-FRONT_800x.png?v=1663686021",
         imageFour: "https://cdn.shopify.com/s/files/1/1429/6260/products/AHTD-TOUR-MERCH-PRODUCT-SHOTS---POSTER-TEE-FRONT_800x.png?v=1663686021",
-        dormName: "AHTD Longsleeve",
+        dormName: "Nugent Hall",
         dormPrice: "60",
         category: "shirt",
         status: "Available",
-        dormAddress: "None",
-        id: "s12",
+        dormAddress: "Nugent Hall, 1999 Burdett Ave, Troy, New York, 12180",
+        id: "nugent_hall",
         rating: "5",
         numRooms: 1,
         numBeds: 2,
         sqft: 140,
         ratingAmt: 120
     }, {
+        //RHAPS A
         imageOne: "https://www.asc-cybernetics.org/2010/wp-content/uploads/2010/07/blitman.jpg",
         imageTwo: "https://cdn.shopify.com/s/files/1/1429/6260/products/AHTD-TOUR-MERCH-PRODUCT-SHOTS---POSTER-TEE-FRONT_800x.png?v=1663686021",
         imageThree: "https://cdn.shopify.com/s/files/1/1429/6260/products/AHTD-TOUR-MERCH-PRODUCT-SHOTS---POSTER-TEE-FRONT_800x.png?v=1663686021",
@@ -213,6 +234,7 @@ export default [
         sqft: 140,
         ratingAmt: 120
     }, {
+        //RHAPS B
         imageOne: "https://www.asc-cybernetics.org/2010/wp-content/uploads/2010/07/blitman.jpg",
         imageTwo: "https://cdn.shopify.com/s/files/1/1429/6260/products/AHTD-TOUR-MERCH-PRODUCT-SHOTS---POSTER-TEE-FRONT_800x.png?v=1663686021",
         imageThree: "https://cdn.shopify.com/s/files/1/1429/6260/products/AHTD-TOUR-MERCH-PRODUCT-SHOTS---POSTER-TEE-FRONT_800x.png?v=1663686021",
@@ -229,22 +251,24 @@ export default [
         sqft: 140,
         ratingAmt: 120
     }, {
-        imageOne: "https://www.asc-cybernetics.org/2010/wp-content/uploads/2010/07/blitman.jpg",
-        imageTwo: "https://cdn.shopify.com/s/files/1/1429/6260/products/AHTD-TOUR-MERCH-PRODUCT-SHOTS---POSTER-TEE-FRONT_800x.png?v=1663686021",
-        imageThree: "https://cdn.shopify.com/s/files/1/1429/6260/products/AHTD-TOUR-MERCH-PRODUCT-SHOTS---POSTER-TEE-FRONT_800x.png?v=1663686021",
-        imageFour: "https://cdn.shopify.com/s/files/1/1429/6260/products/AHTD-TOUR-MERCH-PRODUCT-SHOTS---POSTER-TEE-FRONT_800x.png?v=1663686021",
-        dormName: "AHTD Keychain",
+        //E COMPLEX
+        imageOne: "https://lh3.googleusercontent.com/p/AF1QipNfylBc9c_pBKQ8T4M1AXp3i8jVgvovcbvzFciN=s680-w680-h510", //changed
+        imageTwo: "https://lh3.googleusercontent.com/p/AF1QipM2Jkzw33nfRGvZn1CoA9uknBMIkg7xbnT0w0Yw=s680-w680-h510", //changed
+        imageThree: "https://www.mlbind.com/wp-content/uploads/2018/10/rpi-north-and-e-04.jpg", //changed
+        imageFour: "https://rpirooms.com/room-photos/e-complex-pictures/#jp-carousel-1380", //changed
+        dormName: "E-Complex",
         dormPrice: "30",
         category: "keychain",
         status: "Available",
-        dormAddress: "Limited Edition",
-        id: "keychain1",
+        dormAddress: "E Complex, Sage Ave, Troy, New York, 12180",
+        id: "e_complex",
         rating: "3",
         numRooms: 1,
         numBeds: 2,
         sqft: 140,
         ratingAmt: 120
     }, {
+        //NORTH HALL
         imageOne: "https://www.asc-cybernetics.org/2010/wp-content/uploads/2010/07/blitman.jpg",
         imageTwo: "https://cdn.shopify.com/s/files/1/1429/6260/products/AHTD-TOUR-MERCH-PRODUCT-SHOTS---POSTER-TEE-FRONT_800x.png?v=1663686021",
         imageThree: "https://cdn.shopify.com/s/files/1/1429/6260/products/AHTD-TOUR-MERCH-PRODUCT-SHOTS---POSTER-TEE-FRONT_800x.png?v=1663686021",
